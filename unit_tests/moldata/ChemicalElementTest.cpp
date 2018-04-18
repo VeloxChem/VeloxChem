6a1cf7163583d7f4e25c9f650a97fe160d5d5792
Zilvinas Rinkevicius <rinkevic@kth.se>
2018-04-18 22:12:43 +0200

    - Adding CChemicalElement class for managing data about chemical elements properties.

---
+++ unit_tests/moldata/ChemicalElementTest.cpp
@@ -0,0 +1,112 @@
+//
+//                     V.E.L.O.X. C.H.E.M. MP
+//      ---------------------------------------------------
+//           An Electronic Structure Code for Nanoscale
+//
+//  Created by Zilvinas Rinkevicius (rinkevic@kth.se), KTH, Sweden.
+//  Copyright © 2018 by Velox Chem MP developers. All rights reserved.
+
+#include "ChemicalElementTest.hpp"
+
+#include "ChemicalElement.hpp"
+
+TEST_F(CChemicalElementTest, DefaultConstructor)
+{
+    CChemicalElement achem;
+
+    CChemicalElement bchem(std::string(), 0.0, 0.0, -1);
+
+    ASSERT_EQ(achem, bchem);
+}
+
+TEST_F(CChemicalElementTest, SetAtomType)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType({"CU"}));
+
+    CChemicalElement bchem({"Cu"}, 29.0, 62.929598, 29);
+
+    ASSERT_EQ(achem, bchem);
+
+    ASSERT_FALSE(achem.setAtomType({"XZ"}));
+
+    ASSERT_EQ(achem, bchem);
+}
+
+TEST_F(CChemicalElementTest, SetAtomTypeWithElementNumber)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType(29));
+
+    CChemicalElement bchem({"Cu"}, 29.0, 62.929598, 29);
+
+    ASSERT_EQ(achem, bchem);
+
+    ASSERT_FALSE(achem.setAtomType(99));
+
+    ASSERT_EQ(achem, bchem);
+}
+
+TEST_F(CChemicalElementTest, SetIsotope)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType(29));
+
+    CChemicalElement bchem({"Cu"}, 29.0, 62.929598, 29);
+
+    ASSERT_EQ(achem, bchem);
+
+    ASSERT_TRUE(achem.setIsotope(65));
+
+    CChemicalElement cchem({"Cu"}, 29.0, 64.927790, 29);
+
+    ASSERT_EQ(achem, cchem);
+
+    ASSERT_TRUE(achem.setIsotope(0));
+
+    ASSERT_EQ(achem, bchem);
+
+    ASSERT_FALSE(achem.setIsotope(64));
+
+    ASSERT_EQ(achem, bchem);
+}
+
+TEST_F(CChemicalElementTest, GetName)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType(6));
+
+    ASSERT_EQ(std::string("C"), achem.getName());
+}
+
+TEST_F(CChemicalElementTest, GetIdentifier)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType({"SC"}));
+
+    ASSERT_EQ(21, achem.getIdentifier());
+}
+
+TEST_F(CChemicalElementTest, GetAtomicMass)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType({"H"}));
+
+    ASSERT_NEAR(1.007825, achem.getAtomicMass(), 1.0e-13);
+}
+
+TEST_F(CChemicalElementTest, GetAtomicCharge)
+{
+    CChemicalElement achem;
+
+    ASSERT_TRUE(achem.setAtomType({"N"}));
+
+    ASSERT_NEAR(7.0, achem.getAtomicCharge(), 1.0e-13);
+}
+
---
+++ unit_tests/moldata/ChemicalElementTest.hpp
@@ -0,0 +1,23 @@
+//
+//                     V.E.L.O.X. C.H.E.M. MP
+//      ---------------------------------------------------
+//           An Electronic Structure Code for Nanoscale
+//
+//  Created by Zilvinas Rinkevicius (rinkevic@kth.se), KTH, Sweden.
+//  Copyright © 2018 by Velox Chem MP developers. All rights reserved.
+
+#ifndef ChemicalElementTest_hpp
+#define ChemicalElementTest_hpp
+
+#include "gtest/gtest.h"
+
+class CChemicalElementTest : public ::testing::Test
+{
+protected:
+
+    CChemicalElementTest() {};
+
+    virtual ~CChemicalElementTest() {};
+};
+
+#endif /* ChemicalElementTest_hpp */
