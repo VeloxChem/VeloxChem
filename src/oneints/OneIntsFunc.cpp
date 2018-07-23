c6822425d95fd01d924c97cfd243b639edc944d0
Zilvinas Rinkevicius <rinkevic@kth.se>
2018-07-23 19:31:40 +0200

    - Adding VeloxChem tailored intsfunc subroutines.

--- src/oneints/OneIntsFunc.cpp
+++ src/oneints/OneIntsFunc.cpp
@@ -8,9 +8,223 @@
 
 #include "OneIntsFunc.hpp"
 
+#include "MathFunc.hpp"
 
 namespace intsfunc { // intsfunc namespace
+    
+    void
+    compDistancesAB(      CMemBlock2D<double>& abDistances,
+                    const CGtoBlock&           braGtoBlock,
+                    const CGtoBlock&           ketGtoBlock,
+                    const int32_t              iContrGto)
+    {
+        // set up pointers to primitives data on bra side
+        
+        auto brx = braGtoBlock.getCoordinatesX();
+        
+        auto bry = braGtoBlock.getCoordinatesY();
+        
+        auto brz = braGtoBlock.getCoordinatesZ();
+    
+        auto spos = braGtoBlock.getStartPositions();
+        
+        // compute distances
+        
+        mathfunc::distances(abDistances.data(0),
+                            abDistances.data(1),
+                            abDistances.data(2),
+                            brx[spos[iContrGto]],
+                            bry[spos[iContrGto]],
+                            brz[spos[iContrGto]],
+                            ketGtoBlock.getCoordinatesX(),
+                            ketGtoBlock.getCoordinatesY(),
+                            ketGtoBlock.getCoordinatesZ(),
+                            ketGtoBlock.getNumberOfPrimGtos());
+    }
 
+    void
+    compFactorsForOverlap(      CMemBlock2D<double>& osFactors,
+                          const CGtoBlock&           braGtoBlock,
+                          const CGtoBlock&           ketGtoBlock,
+                          const int32_t              iContrGto)
+    {
+        // set up pointers to primitives data on bra side
+        
+        auto bexp = braGtoBlock.getExponents();
+        
+        auto spos = braGtoBlock.getStartPositions();
+        
+        auto epos = braGtoBlock.getEndPositions();
+        
+        // set up pointers to primitives data on ket side
+        
+        auto kexp = ketGtoBlock.getExponents();
+        
+        auto nprim = ketGtoBlock.getNumberOfPrimGtos();
+        
+        // loop over contracted GTO on bra side
+        
+        int32_t idx = 0;
+        
+        for (int32_t i = spos[iContrGto]; i < epos[iContrGto]; i++)
+        {
+            // set up pointers to Obara-Saika factors
+            
+            auto fx = osFactors.data(2 * idx);
+            
+            auto fz = osFactors.data(2 * idx + 1);
+            
+            auto fb = bexp[i];
+            
+            #pragma omp simd aligned(fx, fz, kexp: VLX_ALIGN)
+            for (int32_t j = 0; j < nprim; j++)
+            {
+                fx[i] = 1.0 / (fb + kexp[j]);
+                
+                fz[i] = fb * kexp[i] * fx[i];
+            }
+            
+            idx++;
+        }
+    }
+    
+    void
+    compDistancesPA(      CMemBlock2D<double>& paDistances,
+                    const CMemBlock2D<double>& abDistances,
+                    const CMemBlock2D<double>& osFactors,
+                    const int32_t              nFactors,
+                    const CGtoBlock&           braGtoBlock,
+                    const CGtoBlock&           ketGtoBlock,
+                    const int32_t              iContrGto)
+    {
+        // skip computation for zero angular momentum on bra side
+        
+        if (braGtoBlock.getAngularMomentum() == 0) return;
+        
+        // set up pointers to primitives data on bra side
+        
+        auto spos = braGtoBlock.getStartPositions();
+        
+        auto epos = braGtoBlock.getEndPositions();
+        
+        // set up pointers to primitives data on ket side
+        
+        auto kexp = ketGtoBlock.getExponents();
+        
+        auto nprim = ketGtoBlock.getNumberOfPrimGtos();
+        
+        // set up pointers to R(AB) distances
+        
+        auto abx = abDistances.data(0);
+        
+        auto aby = abDistances.data(1);
+        
+        auto abz = abDistances.data(2);
+        
+        // loop over contracted GTO on bra side
+        
+        int32_t idx = 0;
+        
+        for (int32_t i = spos[iContrGto]; i < epos[iContrGto]; i++)
+        {
+            // set up pointers to prefactor
+            
+            auto fx = osFactors.data(nFactors * idx);
+           
+            // set up pointers to distances
+            
+            auto pax = paDistances.data(3 * idx);
+            
+            auto pay = paDistances.data(3 * idx + 1);
+            
+            auto paz = paDistances.data(3 * idx + 2);
+            
+            #pragma omp simd aligned(kexp, abx, aby, abz, fx, pax, pay, \
+                                     paz: VLX_ALIGN)
+            for (int32_t j = 0; j < nprim; j++)
+            {
+                double fact = -kexp[i] * fx[i];
+                
+                pax[i] = fact * abx[i];
+                
+                pay[i] = fact * aby[i];
+                
+                paz[i] = fact * abz[i];
+            }
+            
+            idx++;
+        }
+    }
+    
+    void
+    compDistancesPB(      CMemBlock2D<double>& pbDistances,
+                    const CMemBlock2D<double>& abDistances,
+                    const CMemBlock2D<double>& osFactors,
+                    const int32_t              nFactors,
+                    const CGtoBlock&           braGtoBlock,
+                    const CGtoBlock&           ketGtoBlock,
+                    const int32_t              iContrGto)
+    {
+        // skip computation for zero angular momentum on ket side
+        
+        if (ketGtoBlock.getAngularMomentum() == 0) return;
+        
+        // set up pointers to primitives data on bra side
+        
+        auto bexp = braGtoBlock.getExponents();
+        
+        auto spos = braGtoBlock.getStartPositions();
+        
+        auto epos = braGtoBlock.getEndPositions();
+        
+        // set up pointers to primitives data on ket side
+        
+        auto nprim = ketGtoBlock.getNumberOfPrimGtos();
+        
+        // set up pointers to R(AB) distances
+        
+        auto abx = abDistances.data(0);
+        
+        auto aby = abDistances.data(1);
+        
+        auto abz = abDistances.data(2);
+        
+        // loop over contracted GTO on bra side
+        
+        int32_t idx = 0;
+        
+        for (int32_t i = spos[iContrGto]; i < epos[iContrGto]; i++)
+        {
+            // set up pointers to prefactors
+            
+            auto fx = osFactors.data(nFactors * idx);
+            
+            auto fb = bexp[i];
+            
+            // set up pointers to distances
+            
+            auto pbx = pbDistances.data(3 * idx);
+            
+            auto pby = pbDistances.data(3 * idx + 1);
+            
+            auto pbz = pbDistances.data(3 * idx + 2);
+            
+            #pragma omp simd aligned(abx, aby, abz, fx, pbx, pby, pbz: VLX_ALIGN)
+            for (int32_t j = 0; j < nprim; j++)
+            {
+                double fact = fb * fx[i];
+                
+                pbx[i] = fact * abx[i];
+                
+                pby[i] = fact * aby[i];
+                
+                pbz[i] = fact * abz[i];
+            }
+            
+            idx++;
+        }
+    }
+    
     void
     compXiAndZeta(      double* factorsXi,
                         double* factorsZeta,
--- src/oneints/OneIntsFunc.hpp
+++ src/oneints/OneIntsFunc.hpp
@@ -12,7 +12,77 @@
 #include <cstdint>
 #include <cmath>
 
+#include "MemBlock2D.hpp"
+#include "GtoBlock.hpp"
+
 namespace intsfunc { // intsfunc namespace
+    
+    /**
+     Computes distances between specific contracted GTO on bra side with all
+     contracted GTOs on ket side.
+
+     @param abDistances the vector of Cartesian R(AB) = A - B distances.
+     @param braGtoBlock the GTOs block on bra side.
+     @param ketGtoBlock the GTOs block on ket side.
+     @param iContrGto the index of contracted GTO on bra side.
+     */
+    void compDistancesAB(      CMemBlock2D<double>& abDistances,
+                         const CGtoBlock&           braGtoBlock,
+                         const CGtoBlock&           ketGtoBlock,
+                         const int32_t              iContrGto);
+    
+    /**
+     Computes Obara-Saika factors for overlap integrals.
+
+     @param osFactors the vector of Obara-Saika factors.
+     @param braGtoBlock the GTOs block on bra side.
+     @param ketGtoBlock the GTOs block on ket side.
+     @param iContrGto the index of contracted GTO on bra side.
+     */
+    void compFactorsForOverlap(      CMemBlock2D<double>& osFactors,
+                               const CGtoBlock&           braGtoBlock,
+                               const CGtoBlock&           ketGtoBlock,
+                               const int32_t              iContrGto);
+    
+    /**
+     Computes vector of distances between center P of combined primitive GTOs
+     and center A of primitive GTO on bra side.
+     
+     @param paDistances the vector of Cartesian R(PA) = P - A distances.
+     @param abDistances the vector of Cartesian R(AB) = A - B distances.
+     @param osFactors the vector of Obara-Saika factors.
+     @param nFactors the fundamental dimensions of Obara-Saika factors vectors.
+     @param braGtoBlock the GTOs block on bra side.
+     @param ketGtoBlock the GTOs block on ket side.
+     @param iContrGto the index of contracted GTO on bra side.
+     */
+    void compDistancesPA(      CMemBlock2D<double>& paDistances,
+                         const CMemBlock2D<double>& abDistances,
+                         const CMemBlock2D<double>& osFactors,
+                         const int32_t              nFactors,
+                         const CGtoBlock&           braGtoBlock,
+                         const CGtoBlock&           ketGtoBlock,
+                         const int32_t              iContrGto);
+    
+    /**
+     Computes vector of distances between center P of combined primitive GTOs
+     and center B of primitive GTO on ket side.
+     
+     @param pbDistances the vector of Cartesian R(PB) = P - B distances.
+     @param abDistances the vector of Cartesian R(AB) = A - B distances.
+     @param osFactors the vector of Obara-Saika factors.
+     @param nFactors the fundamental dimensions of Obara-Saika factors vectors.
+     @param braGtoBlock the GTOs block on bra side.
+     @param ketGtoBlock the GTOs block on ket side.
+     @param iContrGto the index of contracted GTO on bra side.
+     */
+    void compDistancesPB(      CMemBlock2D<double>& pbDistances,
+                         const CMemBlock2D<double>& abDistances,
+                         const CMemBlock2D<double>& osFactors,
+                         const int32_t              nFactors,
+                         const CGtoBlock&           braGtoBlock,
+                         const CGtoBlock&           ketGtoBlock,
+                         const int32_t              iContrGto);
     
     /**
      Computes vectors of Xi and Zeta factors for combination of specific
