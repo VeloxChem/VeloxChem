43dfad09af8f42693e301e8ffcd05b50f72c329a
Zilvinas Rinkevicius <rinkevic@kth.se>
2018-09-19 18:46:48 +0200

    - Removing not used file.

--- src/orbdata/DensityMatrixType.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  DensityMatrixType.cpp
-//  VeloxChemMP
-//
-//  Created by Zilvinas Rinkevicius on 2018-09-18.
-//  Copyright © 2018 Zilvinas Rinkevicius. All rights reserved.
-//
-
-#include "DensityMatrixType.hpp"
