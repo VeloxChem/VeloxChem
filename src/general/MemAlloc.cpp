87ba55bdff100527d5217cf6933fc646c8b13a4a
Xin Li <lixin.reco@gmail.com>
2018-10-14 14:02:13 +0200

    Now compiles without mkl. Added (1) posix_memalign (2) cblas and lapacke header and (3) -DENABLE_MKL flag

--- src/general/MemAlloc.cpp
+++ src/general/MemAlloc.cpp
@@ -7,21 +7,48 @@
 //  Contact: Zilvinas Rinkevicius (rinkevic@kth.se), KTH, Sweden.
 
 #include "MemAlloc.hpp"
+#include "ErrorHandler.hpp"
 
+#ifdef ENABLE_MKL
 #include "mkl.h"
+#else
+#include <cstdlib>
+#endif
 
 namespace mem { // mem namespace
 
 void*
 malloc(const size_t size)
 {
+    #ifdef ENABLE_MKL
+
     return MKL_malloc(size, VLX_ALIGN);
+
+    #else
+
+    void* ptr = nullptr;
+
+    int ierr = ::posix_memalign(&ptr, VLX_ALIGN, size);
+
+    errors::assertMsgCritical(ierr == 0, "malloc: posix_memalign failed!");
+
+    return ptr;
+
+    #endif
 }
 
 void
 free(void* pointer)
 {
+    #ifdef ENABLE_MKL
+
     if (pointer != nullptr) MKL_free(pointer);
+
+    #else
+
+    if (pointer != nullptr) ::free(pointer);
+
+    #endif
 }
 
 } // mem namespace
