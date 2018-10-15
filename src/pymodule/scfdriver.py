f513fff14a6629063f064911f737deebb470588a
Xin Li <lixin.reco@gmail.com>
2018-10-15 12:28:57 +0200

    Expanded OutputStream class in Python

--- src/pymodule/scfdriver.py
+++ src/pymodule/scfdriver.py
@@ -1,30 +1,31 @@
 class ScfDriver:
+
     def __init__(self):
         # scf type
-        
+
         self.scf_type = "RHF"
-        
+
         # initial guess
-        
+
         self.gues_type = "SAD"
-        
+
         # scf accelerator
         self.acc_type = "DIIS"
         self.max_vecs = 10
-        
+
         # convergence
-        
+
         self.max_iter = 50
-        
+
         # thresholds
-        
+
         self.conv_thresh = 1.0e-6
         self.eri_thresh = 1.0e-12
         self.ovl_thresh = 1.0e-12
-    
+
     def compute(self, ostream):
         # print header to output stream
-        
+
         self.print_header(ostream)
 
     def print_header(self, ostream):
@@ -42,7 +43,8 @@ class ScfDriver:
         ostream.put_header(cur_str.ljust(70))
         cur_str = "Max. Number Of Error Vectors : " + str(self.max_vecs)
         ostream.put_header(cur_str.ljust(70))
-        cur_str = "Convergence Threshold        : " + "{:.1e}".format(self.conv_thresh)
+        cur_str = "Convergence Threshold        : " + \
+            "{:.1e}".format(self.conv_thresh)
         ostream.put_header(cur_str.ljust(70))
         ostream.new_line()
 
@@ -64,5 +66,3 @@ class ScfDriver:
         if self.acc_type == "L2DIIS":
             return "Two Level Direct Inversion of Iterative Subspace"
         return "Unknown Type"
-
-
