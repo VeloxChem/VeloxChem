e5a2a936ce01b91009ee54a00685d74090999c2c
Xin Li <lixin.reco@gmail.com>
2019-01-29 20:09:29 +0100

    Added h2so4 test

--- python_tests/test_rhf.py
+++ python_tests/test_rhf.py
@@ -23,10 +23,12 @@ class TestRHF(unittest.TestCase):
     def test_small_molecules(self):
 
         mol_list = ["h2o", "nh3", "ch4",
-                    "c2h4", "h2o2", "h2se"]
+                    "c2h4", "h2o2", "h2so4",
+                    "h2se"]
 
         scf_ener = [-75.922903268112, -56.195395860545, -40.155481408646,
-                    -78.043152739545, -150.780178806731, -2400.704613197394]
+                    -78.043152739545, -150.780178806731, -698.041440072760,
+                    -2400.704613197391]
 
         for mol_name, e_ref in zip(mol_list, scf_ener):
             e_scf = self.run_rhf(mol_name)
