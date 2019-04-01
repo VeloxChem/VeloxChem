c95dd214439c824ca7700358a2278d8d0708b81e
Xin Li <lixin.reco@gmail.com>
2019-04-01 20:18:40 +0200

    Added python test for polarizability

--- python_tests/test_rspdriver.py
+++ python_tests/test_rspdriver.py
@@ -7,6 +7,7 @@ from veloxchem.molecularorbitals import MolecularOrbitals
 from veloxchem.scfrestdriver import ScfRestrictedDriver
 from veloxchem.tdaexcidriver import TDAExciDriver
 from veloxchem.rspdriver import ResponseDriver
+from veloxchem.lrsolver import LinearResponseSolver
 from veloxchem.mpitask import MpiTask
 
 import numpy as np
@@ -24,12 +25,7 @@ class TestRspDriver(unittest.TestCase):
         scf_drv.compute_task(task)
         mol_orbs = scf_drv.mol_orbs
 
-        # response
-        # TODO: add test for ResponseDriver
-        rsp_drv = ResponseDriver()
-        rsp_drv.compute_task(mol_orbs, task)
-
-        # tda
+        # TDA
         tda_exci = TDAExciDriver(task.mpi_rank, task.mpi_size)
 
         tda_exci.set_number_states(3)
@@ -52,6 +48,38 @@ class TestRspDriver(unittest.TestCase):
             ref_eigs = np.array([0.207436, 0.257474, 0.368358])
 
             self.assertTrue(np.max(np.abs(reigs - ref_eigs)) < 1.0e-6)
+
+        # polarizability
+        lr_solver = LinearResponseSolver()
+
+        lr_solver.set_eri_threshold(1.0e-12)
+        lr_solver.set_solver(1.0e-5, 50)
+
+        lr_prop = lr_solver.compute(task.molecule, task.ao_basis, mol_orbs,
+                                    task.mpi_comm, OutputStream())
+
+        if task.mpi_rank == mpi_master():
+
+            ref_polar = {
+                ('x', 'x', 0): 15.2673,
+                ('x', 'y', 0): 0.0,
+                ('y', 'x', 0): 0.0,
+                ('y', 'y', 0): 23.2248,
+                ('x', 'z', 0): 0.0,
+                ('z', 'x', 0): 0.0,
+                ('y', 'z', 0): -0.759273,
+                ('z', 'y', 0): -0.759273,
+                ('z', 'z', 0): 23.2248,
+            }
+
+            for key, val in lr_prop.items():
+                ref = -ref_polar[key]
+                diff = abs(val - ref)
+                if abs(ref) > 0.0:
+                    rel_diff = abs(diff / ref)
+                    self.assertTrue(rel_diff < 5.0e-6)
+                else:
+                    self.assertTrue(diff < 1.0e-10)
 
         task.finish()
 
