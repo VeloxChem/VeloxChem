bc79017fb83d325e5ed467aad8b757873ade54d4
Xin Li <lixin.reco@gmail.com>
2019-05-15 23:00:34 +0200

    Exciton model: parallelization on single communicator

--- src/pymodule/excitondriver.py
+++ src/pymodule/excitondriver.py
@@ -60,7 +60,8 @@ class ExcitonModelDriver:
 
             # monomer molecule
             monomer = molecule.get_sub_molecule(start_indices[ind], natoms[ind])
-            self.ostream.print_block(monomer.get_string())
+            if self.rank == mpi_master():
+                self.ostream.print_block(monomer.get_string())
 
             # SCF calculation
             scf_drv = ScfRestrictedDriver(self.comm, self.ostream)
@@ -72,8 +73,9 @@ class ExcitonModelDriver:
             })
             scf_drv.compute(monomer, basis, min_basis)
 
-            self.monomers[ind]['C'] = scf_drv.scf_tensors['C'].copy()
-            self.monomers[ind]['F'] = scf_drv.scf_tensors['F'][0].copy()
+            if self.rank == mpi_master():
+                self.monomers[ind]['C'] = scf_drv.scf_tensors['C'].copy()
+                self.monomers[ind]['F'] = scf_drv.scf_tensors['F'][0].copy()
 
             # TDA calculation
             abs_spec = Absorption({
@@ -85,82 +87,88 @@ class ExcitonModelDriver:
             })
             abs_spec.init_driver(self.comm, self.ostream)
             abs_spec.compute(monomer, basis, scf_drv.scf_tensors)
+
             if self.rank == mpi_master():
                 abs_spec.print_property(self.ostream)
+                self.monomers[ind]['exc_energies'] = abs_spec.get_property(
+                    'eigenvalues')
+                self.monomers[ind]['exc_vectors'] = abs_spec.get_property(
+                    'eigenvectors')
 
-            self.monomers[ind]['exc_energies'] = abs_spec.get_property(
-                'eigenvalues')
-            self.monomers[ind]['exc_vectors'] = abs_spec.get_property(
-                'eigenvectors')
+        for ind_A in range(nfragments):
+            monomer_a = molecule.get_sub_molecule(start_indices[ind_A],
+                                                  natoms[ind_A])
 
-        for ind_a in range(nfragments):
-            monomer_a = molecule.get_sub_molecule(start_indices[ind_a],
-                                                  natoms[ind_a])
-
-            for ind_b in range(ind_a + 1, nfragments):
-                monomer_b = molecule.get_sub_molecule(start_indices[ind_b],
-                                                      natoms[ind_b])
+            for ind_B in range(ind_A + 1, nfragments):
+                monomer_b = molecule.get_sub_molecule(start_indices[ind_B],
+                                                      natoms[ind_B])
 
                 # dimer molecule
                 dimer = Molecule(monomer_a, monomer_b)
-                self.ostream.print_block(dimer.get_string())
+                if self.rank == mpi_master():
+                    self.ostream.print_block(dimer.get_string())
 
                 # 1e integrals
                 kin_drv = KineticEnergyIntegralsDriver(self.comm)
-                kin_mat = kin_drv.compute(dimer, basis)
-
                 npot_drv = NuclearPotentialIntegralsDriver(self.comm)
-                npot_mat = npot_drv.compute(dimer, basis)
 
-                # get indices of monomer AOs in dimer
-                ao_inds_1, ao_inds_2 = get_dimer_ao_indices(
-                    monomer_a, monomer_b, basis, basis)
+                if self.rank == mpi_master():
+                    kin_mat = kin_drv.compute(dimer, basis)
+                    npot_mat = npot_drv.compute(dimer, basis)
 
-                # assemble MO coefficient matrix
+                    # get indices of monomer AOs in dimer
+                    ao_inds_A, ao_inds_B = get_dimer_ao_indices(
+                        monomer_a, monomer_b, basis, basis)
 
-                #         occ.A     occ.B     vir.A     vir.B
-                #      +---------+---------+---------+---------+
-                #      |         |         |         |         |
-                # ao.A |  CoccA  |         |  CvirA  |         |
-                #      |         |         |         |         |
-                #      +---------+---------+---------+---------+
-                #      |         |         |         |         |
-                # ao.B |         |  CoccB  |         |  CvirB  |
-                #      |         |         |         |         |
-                #      +---------+---------+---------+---------+
+                    # assemble MO coefficient matrix
 
-                CA = self.monomers[ind_a]['C']
-                CB = self.monomers[ind_b]['C']
+                    #         occ.A     occ.B     vir.A     vir.B
+                    #      +---------+---------+---------+---------+
+                    #      |         |         |         |         |
+                    # ao.A |  CoccA  |         |  CvirA  |         |
+                    #      |         |         |         |         |
+                    #      +---------+---------+---------+---------+
+                    #      |         |         |         |         |
+                    # ao.B |         |  CoccB  |         |  CvirB  |
+                    #      |         |         |         |         |
+                    #      +---------+---------+---------+---------+
 
-                nao_A = CA.shape[0]
-                nao_B = CB.shape[0]
-                nmo_A = CA.shape[1]
-                nmo_B = CB.shape[1]
+                    CA = self.monomers[ind_A]['C']
+                    CB = self.monomers[ind_B]['C']
 
-                nocc_A = monomer_a.number_of_alpha_electrons()
-                nocc_B = monomer_b.number_of_alpha_electrons()
-                nvir_A = nmo_A - nocc_A
-                nvir_B = nmo_B - nocc_B
+                    nao_A = CA.shape[0]
+                    nao_B = CB.shape[0]
+                    nmo_A = CA.shape[1]
+                    nmo_B = CB.shape[1]
 
-                nocc = nocc_A + nocc_B
-                nvir = nvir_A + nvir_B
+                    nocc_A = monomer_a.number_of_alpha_electrons()
+                    nocc_B = monomer_b.number_of_alpha_electrons()
+                    nvir_A = nmo_A - nocc_A
+                    nvir_B = nmo_B - nocc_B
 
-                mo = np.zeros((nao_A + nao_B, nmo_A + nmo_B))
+                    nocc = nocc_A + nocc_B
+                    nvir = nvir_A + nvir_B
 
-                for row in range(nao_A):
-                    mo[ao_inds_1[row], :nocc_A] = CA[row, :nocc_A]
-                    mo[ao_inds_1[row], nocc:nocc + nvir_A] = CA[row, nocc_A:]
+                    mo = np.zeros((nao_A + nao_B, nmo_A + nmo_B))
 
-                for row in range(nao_B):
-                    mo[ao_inds_2[row], nocc_A:nocc] = CB[row, :nocc_B]
-                    mo[ao_inds_2[row], nocc + nvir_A:] = CB[row, nocc_B:]
+                    for row in range(nao_A):
+                        mo[ao_inds_A[row], :nocc_A] = CA[row, :nocc_A]
+                        mo[ao_inds_A[row], nocc:nocc +
+                           nvir_A] = CA[row, nocc_A:]
 
-                mo_occ = mo[:, :nocc]
-                mo_vir = mo[:, nocc:]
+                    for row in range(nao_B):
+                        mo[ao_inds_B[row], nocc_A:nocc] = CB[row, :nocc_B]
+                        mo[ao_inds_B[row], nocc + nvir_A:] = CB[row, nocc_B:]
 
-                # compute density matrix
-                dens = np.matmul(mo_occ, mo_occ.T)
-                dens_mat = AODensityMatrix([dens], denmat.rest)
+                    mo_occ = mo[:, :nocc]
+                    mo_vir = mo[:, nocc:]
+
+                    # compute density matrix
+                    dens = np.matmul(mo_occ, mo_occ.T)
+                    dens_mat = AODensityMatrix([dens], denmat.rest)
+                else:
+                    dens_mat = AODensityMatrix()
+
                 dens_mat.broadcast(self.rank, self.comm)
 
                 # compute Fock matrix
@@ -173,48 +181,55 @@ class ExcitonModelDriver:
                 eri_drv.compute(fock_mat, dens_mat, dimer, basis, screening)
                 fock_mat.reduce_sum(self.rank, self.nodes, self.comm)
 
-                fock_mo = np.matmul(mo.T, np.matmul(fock_mat.to_numpy(0), mo))
-                fock_occ = fock_mo[:nocc, :nocc]
-                fock_vir = fock_mo[nocc:, nocc:]
+                if self.rank == mpi_master():
+                    fock_mo = np.matmul(mo.T, np.matmul(fock_mat.to_numpy(0),
+                                                        mo))
+                    fock_occ = fock_mo[:nocc, :nocc]
+                    fock_vir = fock_mo[nocc:, nocc:]
 
-                # compute dimer energy
-                dimer_energy = dimer.nuclear_repulsion_energy()
-                dimer_energy += fock_mat.get_energy(0, dens_mat, 0)
-                dimer_energy += 2.0 * kin_mat.get_energy(dens_mat, 0)
-                dimer_energy -= 2.0 * npot_mat.get_energy(dens_mat, 0)
+                    # compute dimer energy
+                    dimer_energy = dimer.nuclear_repulsion_energy()
+                    dimer_energy += fock_mat.get_energy(0, dens_mat, 0)
+                    dimer_energy += 2.0 * kin_mat.get_energy(dens_mat, 0)
+                    dimer_energy -= 2.0 * npot_mat.get_energy(dens_mat, 0)
 
-                valstr = 'Dimer Energy:{:20.10f} au'.format(dimer_energy)
-                self.ostream.print_header(valstr.ljust(92))
-                self.ostream.print_blank()
+                    valstr = 'Dimer Energy:{:20.10f} au'.format(dimer_energy)
+                    self.ostream.print_header(valstr.ljust(92))
+                    self.ostream.print_blank()
 
-                # assemble TDA CI vectors
+                    # assemble TDA CI vectors
 
-                #          vir.A     vir.B                vir.A     vir.B
-                #       +---------+---------+          +---------+---------+
-                # occ.A |  LE(A)  |         |    occ.A |         |         |
-                #       +---------+---------+          +---------+---------+
-                # occ.B |         |         |    occ.B |         |  LE(B)  |
-                #       +---------+---------+          +---------+---------+
+                    #          vir.A     vir.B                vir.A     vir.B
+                    #       +---------+---------+          +---------+---------+
+                    # occ.A |  LE(A)  |         |    occ.A |         |         |
+                    #       +---------+---------+          +---------+---------+
+                    # occ.B |         |         |    occ.B |         |  LE(B)  |
+                    #       +---------+---------+          +---------+---------+
 
-                #          vir.A     vir.B                vir.A     vir.B
-                #       +---------+---------+          +---------+---------+
-                # occ.A |         |  CT(AB) |    occ.A |         |         |
-                #       +---------+---------+          +---------+---------+
-                # occ.B |         |         |    occ.B |  CT(BA) |         |
-                #       +---------+---------+          +---------+---------+
+                    #          vir.A     vir.B                vir.A     vir.B
+                    #       +---------+---------+          +---------+---------+
+                    # occ.A |         |  CT(AB) |    occ.A |         |         |
+                    #       +---------+---------+          +---------+---------+
+                    # occ.B |         |         |    occ.B |  CT(BA) |         |
+                    #       +---------+---------+          +---------+---------+
 
-                vecs_A = self.monomers[ind_a]['exc_vectors']
-                vecs_B = self.monomers[ind_b]['exc_vectors']
+                    vecs_A = self.monomers[ind_A]['exc_vectors']
+                    vecs_B = self.monomers[ind_B]['exc_vectors']
 
-                for sA in range(vecs_A.shape[1]):
-                    vec_A = vecs_A[:, sA].reshape(nocc_A, nvir_A)
+                for sA in range(self.nstates):
 
-                    CI_vec_A = np.zeros((nocc, nvir))
-                    CI_vec_A[:nocc_A, :nvir_A] = vec_A[:, :]
+                    if self.rank == mpi_master():
+                        vec_A = vecs_A[:, sA].reshape(nocc_A, nvir_A)
 
-                    tdens = np.matmul(mo_occ, np.matmul(CI_vec_A, mo_vir.T))
+                        CI_vec_A = np.zeros((nocc, nvir))
+                        CI_vec_A[:nocc_A, :nvir_A] = vec_A[:, :]
 
-                    tdens_mat = AODensityMatrix([tdens], denmat.rest)
+                        tdens = np.matmul(mo_occ, np.matmul(CI_vec_A, mo_vir.T))
+
+                        tdens_mat = AODensityMatrix([tdens], denmat.rest)
+                    else:
+                        tdens_mat = AODensityMatrix()
+
                     tdens_mat.broadcast(self.rank, self.comm)
 
                     tfock_mat = AOFockMatrix(tdens_mat)
@@ -223,23 +238,27 @@ class ExcitonModelDriver:
                     eri_drv.compute(tfock_mat, tdens_mat, dimer, basis,
                                     screening)
                     tfock_mat.reduce_sum(self.rank, self.nodes, self.comm)
-                    tfock = tfock_mat.to_numpy(0)
 
-                    sigma_vec_A = np.matmul(mo_occ.T, np.matmul(tfock, mo_vir))
+                    if self.rank == mpi_master():
+                        tfock = tfock_mat.to_numpy(0)
 
-                    sigma_vec_A -= np.matmul(fock_occ, CI_vec_A)
-                    sigma_vec_A += np.matmul(CI_vec_A, fock_vir)
+                        sigma_vec_A = np.matmul(mo_occ.T,
+                                                np.matmul(tfock, mo_vir))
 
-                    for sB in range(vecs_B.shape[1]):
-                        vec_B = vecs_B[:, sB].reshape(nocc_B, nvir_B)
+                        sigma_vec_A -= np.matmul(fock_occ, CI_vec_A)
+                        sigma_vec_A += np.matmul(CI_vec_A, fock_vir)
 
-                        CI_vec_B = np.zeros((nocc, nvir))
-                        CI_vec_B[nocc_A:, nvir_A:] = vec_B[:, :]
+                        for sB in range(self.nstates):
+                            vec_B = vecs_B[:, sB].reshape(nocc_B, nvir_B)
 
-                        coupling = np.sum(sigma_vec_A * CI_vec_B)
-                        valstr = 'LE-LE coupling:'
-                        valstr += '  {}e({}){}g  {}g{}e({}) {:20.12f}'.format(
-                            ind_a + 1, sA + 1, ind_b + 1, ind_a + 1, ind_b + 1,
-                            sB + 1, coupling)
-                        self.ostream.print_header(valstr.ljust(92))
-                    self.ostream.print_blank()
+                            CI_vec_B = np.zeros((nocc, nvir))
+                            CI_vec_B[nocc_A:, nvir_A:] = vec_B[:, :]
+
+                            coupling = np.sum(sigma_vec_A * CI_vec_B)
+                            valstr = 'LE-LE coupling:'
+                            valstr += '  {}e({}){}g  {}g{}e({})'.format(
+                                ind_A + 1, sA + 1, ind_B + 1, ind_A + 1,
+                                ind_B + 1, sB + 1)
+                            valstr += '  {:20.12f}'.format(coupling)
+                            self.ostream.print_header(valstr.ljust(92))
+                        self.ostream.print_blank()
