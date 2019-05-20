c95f35dcdf3db6d277c8545ebe5282b4ec90f8cc
Xin Li <lixin.reco@gmail.com>
2019-05-20 20:30:13 +0200

    Minor improvement in setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,35 @@ from setuptools.command.build_py import build_py as SetuptoolsBuildPy
 from setuptools.command.install import install as SetuptoolsInstall
 import subprocess
 import sys
+import os
 
 
 class MyBuildPy(SetuptoolsBuildPy):
 
     def run(self):
-        if run_process('make -C src'):
+        if self.make_veloxchem():
             SetuptoolsBuildPy.run(self)
         else:
             print('Error: failed to build veloxchem')
             sys.exit(1)
 
+    def make_veloxchem(self):
+        process = subprocess.Popen('make -C src'.split(),
+                                   stderr=subprocess.STDOUT)
+        return process.wait() == 0
+
 
 class MyInstall(SetuptoolsInstall):
 
     def run(self):
-        if run_process('mkdir -p build/python/veloxchem'):
-            SetuptoolsInstall.run(self)
-        else:
-            print('Error: failed to create folder build/python/veloxchem')
-            sys.exit(1)
-
-
-def run_process(command):
-    process = subprocess.Popen(command.split(), stderr=subprocess.STDOUT)
-    return process.wait() == 0
+        package_dir = os.path.join('build', 'python', 'veloxchem')
+        if not os.path.isdir(package_dir):
+            try:
+                os.makedirs(package_dir)
+            except IOError:
+                raise IOError(
+                    'Unable to create package dir {}'.format(package_dir))
+        SetuptoolsInstall.run(self)
 
 
 setup(
@@ -37,9 +41,15 @@ setup(
     package_dir={'veloxchem': 'build/python/veloxchem'},
     package_data={'veloxchem': ['veloxchemlib.so', 'basis/*']},
     scripts=['build/bin/VeloxChemMain.py'],
+    python_requires='>=3.4',
+    install_requires=[
+        'mpi4py>=3.0',
+        'numpy>=1.13',
+        'h5py>=2.8',
+    ],
     cmdclass={
         'build_py': MyBuildPy,
-        'install': MyInstall
+        'install': MyInstall,
     },
     description='VeloxChem: an electronic structure code',
 )
