d20825d60b8589f07fddd2ceb0aaf62b6088af28
Olav Vahtras <vahtras@kth.se>
2019-06-04 15:34:32 +0200

    update server links

--- docs/server.py
+++ docs/server.py
@@ -4,16 +4,16 @@ import flask
 
 app = flask.Flask(
     __name__, 
-    static_folder='doc/_build/html',
+    static_folder='_build/html',
 )
 
 @app.route('/')
 def home():
-    return flask.send_from_directory('doc/_build/html',  'index.html')
+    return flask.send_from_directory('_build/html',  'index.html')
 
 @app.route('/<path:path>')
 def page(path):
-    return flask.send_from_directory('doc/_build/html',  path)
+    return flask.send_from_directory('_build/html',  path)
 
 if __name__ == "__main__":
     app.run(debug=True)
