e3664db8491c7aa4d00934aa2e8e4202a84bec1a
Olav Vahtras <vahtras@kth.se>
2019-06-05 08:57:37 +0200

    include mocks for docs

---
+++ docs/mocks/veloxchemlib.py
@@ -0,0 +1,43 @@
+# Define names for sphinx-api
+
+from unittest.mock import Mock
+
+AtomBasis = Mock()
+BasisFunction = Mock()
+OverlapIntegralsDriver = Mock()
+KineticEnergyIntegralsDriver = Mock()
+NuclearPotentialIntegralsDriver = Mock()
+ElectricDipoleIntegralsDriver = Mock()
+ElectronRepulsionIntegralsDriver = Mock()
+SADGuessDriver = Mock()
+TwoIndexes = Mock()
+MOIntsBatch = Mock()
+ExcitationVector = Mock()
+
+
+mpi_master = Mock()
+mpi_initialized = Mock()
+bohr_in_angstroms = Mock()
+hartree_in_ev = Mock()
+mathconst_pi = Mock()
+
+
+denmat = Mock()
+ericut = Mock()
+molorb = Mock()
+moints = Mock()
+
+assert_msg_critical = Mock()
+Molecule = Mock()
+MolecularBasis = Mock()
+ChemicalElement = Mock()
+to_angular_momentum = Mock()
+AODensityMatrix = Mock()
+MolecularOrbitals = Mock()
+AOFockMatrix = Mock()
+fockmat = Mock()
+VisualizationDriver = Mock()
+CubicGrid = Mock()
+get_dimer_ao_indices = Mock()
+szblock = Mock()
+TDASigmaVectorDriver = Mock()
