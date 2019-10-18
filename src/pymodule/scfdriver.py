e1da3cb58979c5ac40134a0da067d3e6ab76d284
Xin Li <lixin.reco@gmail.com>
2019-10-18 13:36:06 +0200

    Temporarily disabled split communicators

--- src/pymodule/scfdriver.py
+++ src/pymodule/scfdriver.py
@@ -720,7 +720,7 @@ class ScfDriver:
             The AO Kohn-Sham (Vxc) matrix.
         """
 
-        if (self.dft or self.pe) and self.nodes >= 8:
+        if (self.dft or self.pe) and self.nodes >= 9999:
             vxc_mat, e_pe, V_pe = self.comp_2e_fock_split_comm(
                 fock_mat, den_mat, molecule, basis, screening)
         else:
