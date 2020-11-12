3cf0f9505d0705224ab03f8895d46c9a1a1722b2
Xin Li <lixin.reco@gmail.com>
2020-11-12 13:40:18 +0100

    updated test_nto.py and test_detach_attach.py

--- python_tests/test_nto.py
+++ python_tests/test_nto.py
@@ -10,21 +10,29 @@ from veloxchem.molecularorbitals import MolecularOrbitals
 from veloxchem.visualizationdriver import VisualizationDriver
 from veloxchem.scfrestdriver import ScfRestrictedDriver
 from veloxchem.tdaexcidriver import TDAExciDriver
+from veloxchem.lreigensolver import LinearResponseEigenSolver
 
 
 class TestNTO(unittest.TestCase):
 
-    def run_nto(self, inpfile, ref_eig_vals, ref_nto_vals, ref_cube_vals):
+    def run_nto(self, inpfile, xcfun_label, ref_eig_vals, ref_nto_vals,
+                ref_cube_vals, flag):
 
         task = MpiTask([inpfile, None], MPI.COMM_WORLD)
         task.input_dict['scf']['checkpoint_file'] = None
+
+        if xcfun_label is not None:
+            task.input_dict['method_settings']['xcfun'] = xcfun_label
 
         scf_drv = ScfRestrictedDriver(task.mpi_comm, task.ostream)
         scf_drv.update_settings(task.input_dict['scf'],
                                 task.input_dict['method_settings'])
         scf_drv.compute(task.molecule, task.ao_basis, task.min_basis)
 
-        rsp_drv = TDAExciDriver(task.mpi_comm, task.ostream)
+        if flag == 'tda':
+            rsp_drv = TDAExciDriver(task.mpi_comm, task.ostream)
+        elif flag == 'rpa':
+            rsp_drv = LinearResponseEigenSolver(task.mpi_comm, task.ostream)
         rsp_drv.update_settings({'nstates': ref_eig_vals.shape[0]},
                                 task.input_dict['method_settings'])
         rsp_results = rsp_drv.compute(task.molecule, task.ao_basis,
@@ -47,7 +55,12 @@ class TestNTO(unittest.TestCase):
 
         for s in range(ref_eig_vals.shape[0]):
             if task.mpi_rank == mpi_master():
-                t_mat = eig_vecs[:, s].reshape(mo_occ.shape[1], mo_vir.shape[1])
+                eig_vec = eig_vecs[:, s].copy()
+                if flag == 'tda':
+                    t_mat = eig_vec.reshape(mo_occ.shape[1], mo_vir.shape[1])
+                elif flag == 'rpa':
+                    t_mat = eig_vec[:eig_vec.shape[0] // 2].reshape(
+                        mo_occ.shape[1], mo_vir.shape[1]) * np.sqrt(2.0)
                 lam_diag, nto_mo = rsp_drv.get_nto(t_mat, mo_occ, mo_vir)
             else:
                 lam_diag = None
@@ -87,8 +100,11 @@ class TestNTO(unittest.TestCase):
             nto_vals = np.array(nto_vals)
             cube_vals = np.array(cube_vals)
 
-            self.assertTrue(np.max(np.abs(eig_vals - ref_eig_vals)) < 1.0e-6)
-            self.assertTrue(np.max(np.abs(nto_vals - ref_nto_vals)) < 1.0e-4)
+            thresh = 1.0e-6 if xcfun_label is None else 1.0e-5
+            self.assertTrue(np.max(np.abs(eig_vals - ref_eig_vals)) < thresh)
+
+            thresh = 1.0e-4 if xcfun_label is None else 1.0e-3
+            self.assertTrue(np.max(np.abs(nto_vals - ref_nto_vals)) < thresh)
 
             for s in range(cube_vals.shape[0]):
                 if np.vdot(cube_vals[s, :], ref_cube_vals[s, :]) < 0.0:
@@ -100,13 +116,16 @@ class TestNTO(unittest.TestCase):
         here = Path(__file__).parent
         inpfile = str(here / 'inputs' / 'c2h4.inp')
 
+        xcfun_label = None
+
         raw_vals = """
-            0.316434
-            0.346741
-            0.348663
-            0.358313
-            0.386238
+            0.31643416
+            0.34674141
+            0.34866292
+            0.35831265
+            0.38623776
         """
+
         eig_vals = np.array([float(x) for x in raw_vals.split()])
         nstates = eig_vals.size
 
@@ -165,7 +184,61 @@ class TestNTO(unittest.TestCase):
         cube_vals = np.array([float(x) for x in raw_vals.split()])
         cube_vals = cube_vals.reshape(nstates * 2, -1)
 
-        self.run_nto(inpfile, eig_vals, nto_vals, cube_vals)
+        self.run_nto(inpfile, xcfun_label, eig_vals, nto_vals, cube_vals, 'tda')
+
+    def test_nto_rpa(self):
+
+        here = Path(__file__).parent
+        inpfile = str(here / 'inputs' / 'c2h4.inp')
+
+        xcfun_label = 'b3lyp'
+
+        raw_vals = """
+            0.30465317
+            0.30537184
+            0.31770840
+        """
+        eig_vals = np.array([float(x) for x in raw_vals.split()])
+        nstates = eig_vals.size
+
+        raw_vals = """
+            0.9816 -0.3554 0.0094
+            0.9900 -0.2784 0.0100
+            0.9977 -0.2784 0.0861
+        """
+        nto_vals = np.array([float(x) for x in raw_vals.split()])
+        nto_vals = nto_vals.reshape(nstates, -1)
+
+        raw_vals = """
+            3.21430E-03 -3.21430E-03 -1.03578E-02
+           -6.22010E-03  6.22010E-03  2.00043E-02
+            3.10657E-03 -3.10657E-03 -9.99144E-03
+           -6.01501E-03  6.01501E-03  1.93083E-02
+            4.77717E-03 -4.77717E-03 -1.52606E-02
+            4.62157E-03 -4.62157E-03 -1.47339E-02
+           -9.24233E-03  9.24233E-03  2.94651E-02
+           -8.94621E-03  8.94621E-03  2.84649E-02
+            3.05252E-02  3.05252E-02  3.41449E-02
+            2.99013E-02  2.99013E-02  3.33239E-02
+           -5.98010E-02 -5.98010E-02 -6.66455E-02
+           -5.85944E-02 -5.85944E-02 -6.50684E-02
+            4.36168E-03 -4.36168E-03 -1.39084E-02
+            4.22290E-03 -4.22290E-03 -1.34388E-02
+           -8.44485E-03  8.44485E-03  2.68744E-02
+           -8.18069E-03  8.18069E-03  2.59826E-02
+            3.05252E-02  3.05252E-02  3.41449E-02
+            2.99013E-02  2.99013E-02  3.33239E-02
+           -5.98010E-02 -5.98010E-02 -6.66455E-02
+           -5.85944E-02 -5.85944E-02 -6.50684E-02
+            1.78746E-02  1.78746E-02  2.28482E-02
+            1.71020E-02  1.71020E-02  2.14232E-02
+            1.64037E-02  1.64037E-02  2.07118E-02
+            1.57078E-02  1.57078E-02  1.94172E-02
+        """
+        cube_vals = np.array([float(x) for x in raw_vals.split()])
+        cube_vals = cube_vals.reshape(nstates * 2, -1)
+
+        self.run_nto(inpfile, xcfun_label, eig_vals, nto_vals, cube_vals, 'rpa')
 
 
 if __name__ == "__main__":
