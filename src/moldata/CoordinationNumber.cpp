47152375336e8ff3b4ae596dc2e2b98fe9bd650d
Xin Li <lixin.reco@gmail.com>
2020-12-11 21:40:09 +0100

    updated contact info in src/moldata

--- src/moldata/CoordinationNumber.cpp
+++ src/moldata/CoordinationNumber.cpp
@@ -1,10 +1,10 @@
 //
-//                             VELOXCHEM
+//                           VELOXCHEM 1.0-RC
 //      ---------------------------------------------------
 //                     An Electronic Structure Code
 //
 //  Copyright © 2018-2020 by VeloxChem developers. All rights reserved.
-//  Contact: Zilvinas Rinkevicius (rinkevic@kth.se), KTH, Sweden.
+//  Contact: https://veloxchem.org/contact
 
 #include "CoordinationNumber.hpp"
 
