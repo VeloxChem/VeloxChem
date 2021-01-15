41d01b4a3af0350f13f3cc84ac153ee67075d3ba
Xin Li <lixin.reco@gmail.com>
2021-01-15 13:43:00 +0100

    updated test_visualization.py

--- python_tests/test_visualization.py
+++ python_tests/test_visualization.py
@@ -34,8 +34,10 @@ class TestVisualization(unittest.TestCase):
         vis_drv.compute(grid, task.molecule, task.ao_basis, mol_orbs, homo,
                         'alpha')
 
-        mo_val = vis_drv.get_mo([[0.3, 0.6, 0.9]], task.molecule, task.ao_basis,
-                                mol_orbs, homo, 'alpha')
+        points = [[0.3 + 1.0 * ix, 0.6 + 1.0 * iy, 0.9 + 1.0 * iz]
+                  for ix in range(2) for iy in range(3) for iz in range(3)]
+        mo_val = vis_drv.get_mo(points, task.molecule, task.ao_basis, mol_orbs,
+                                homo, 'alpha')
 
         if task.mpi_rank == mpi_master():
             homo_values = grid.values_to_numpy()
@@ -45,18 +47,20 @@ class TestVisualization(unittest.TestCase):
                 6.37159e-03, 1.33161e-02, 6.65229e-03, 3.03229e-03, 1.59826e-01,
                 7.64720e-02, 3.17590e-02, 8.77249e-02, 4.56304e-02, 1.99448e-02,
                 3.70652e-02, 2.14514e-02, 1.03838e-02
-            ]).reshape((2, 3, 3))
+            ]).reshape(2, 3, 3)
 
             homo_diff = np.abs(homo_values) - np.abs(homo_ref)
             homo_diff_rel = homo_diff / np.abs(homo_ref)
             self.assertTrue(np.max(homo_diff_rel) < 1.0e-5)
 
-            self.assertAlmostEqual(homo_values[0, 0, 0], mo_val[0], 8)
+            mo_val = np.array(mo_val).reshape(2, 3, 3)
+            self.assertTrue(
+                np.max(np.abs(np.abs(homo_values) - np.abs(mo_val))) < 1.0e-8)
 
         vis_drv.compute(grid, task.molecule, task.ao_basis, density, 0, 'alpha')
 
-        den_val = vis_drv.get_density([[0.3, 0.6, 0.9]], task.molecule,
-                                      task.ao_basis, density, 0, 'alpha')
+        den_val = vis_drv.get_density(points, task.molecule, task.ao_basis,
+                                      density, 0, 'alpha')
 
         if task.mpi_rank == mpi_master():
             dens_alpha = grid.values_to_numpy()
@@ -67,29 +71,31 @@ class TestVisualization(unittest.TestCase):
                 1.66540e-02, 7.19205e-02, 1.21828e-02, 3.05223e-03, 1.01686e-01,
                 4.68902e-02, 2.50173e-02, 4.55977e-02, 1.69215e-02, 7.68553e-03,
                 2.35932e-02, 6.38199e-03, 1.90608e-03
-            ]).reshape((2, 3, 3))
+            ]).reshape(2, 3, 3)
 
             dens_diff = dens_total - dens_ref
             dens_diff_rel = dens_diff / np.abs(dens_ref)
             self.assertTrue(np.max(dens_diff_rel) < 1.0e-5)
 
-            self.assertAlmostEqual(dens_alpha[0, 0, 0], den_val[0], 8)
+            den_val = np.array(den_val).reshape(2, 3, 3)
+            self.assertTrue(np.max(np.abs(dens_alpha - den_val)) < 1.0e-8)
 
-        twoe_val_aa = vis_drv.get_two_particle_density([[0.3, 0.6, 0.9]],
-                                                       [[0.3, 0.6, 0.9]],
+        twoe_val_aa = vis_drv.get_two_particle_density(points, points,
                                                        task.molecule,
                                                        task.ao_basis, density,
                                                        0, 'alpha', 'alpha')
 
-        twoe_val_ab = vis_drv.get_two_particle_density([[0.3, 0.6, 0.9]],
-                                                       [[0.3, 0.6, 0.9]],
+        twoe_val_ab = vis_drv.get_two_particle_density(points, points,
                                                        task.molecule,
                                                        task.ao_basis, density,
                                                        0, 'alpha', 'beta')
 
         if task.mpi_rank == mpi_master():
-            self.assertAlmostEqual(twoe_val_aa[0], 0.0, 8)
-            self.assertAlmostEqual(twoe_val_ab[0], den_val[0]**2, 8)
+            twoe_val_aa = np.array(twoe_val_aa).reshape(2, 3, 3)
+            twoe_val_ab = np.array(twoe_val_ab).reshape(2, 3, 3)
+
+            self.assertTrue(np.max(np.abs(twoe_val_aa) < 1.0e-8))
+            self.assertTrue(np.max(np.abs(twoe_val_ab - den_val**2) < 1.0e-8))
 
         task.finish()
 
