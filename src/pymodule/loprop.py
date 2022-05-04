606114a289c30e4544c54b16fa8dbb2052c0f2a7
Xin Li <lixin.reco@gmail.com>
2022-05-04 13:30:51 +0200

    minor update in loprop output

--- src/pymodule/loprop.py
+++ src/pymodule/loprop.py
@@ -333,7 +333,7 @@ class LoPropDriver:
                                                molecule_coord[b][i])
                         dRab[b][a][i] = -dRab[a][b][i]
 
-            # charge transfer from bond polarisability
+            # charge transfer from bond polarizability
             bond_polarizabilities = np.zeros((3, 3, natoms, natoms))
             for i in range(3):
                 for j in range(3):
@@ -546,7 +546,7 @@ class LoPropDriver:
         self.ostream.print_header('=' * (len(title) + 2))
         self.ostream.print_blank()
 
-        title = 'Molecular Polarisabilities'
+        title = 'Molecular Polarizabilities'
         self.ostream.print_header(title)
         self.ostream.print_header('-' * len(title))
 
