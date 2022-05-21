885daedc4a409c579cdbbeb3cae41a95fd948a85
Karan <karana@kth.se>
2022-05-21 10:52:39 +0200

    added test for shg_reduced

---
+++ python_tests/test_shg_reduced.py
@@ -0,0 +1,85 @@
+from pathlib import Path
+import pytest
+
+from veloxchem.veloxchemlib import mpi_master
+from veloxchem.mpitask import MpiTask
+from veloxchem.scfrestdriver import ScfRestrictedDriver
+from veloxchem.rspshg import SHG
+
+
+@pytest.mark.solvers
+class TestSHG:
+
+    def run_scf(self, task):
+
+        scf_drv = ScfRestrictedDriver(task.mpi_comm, task.ostream)
+        scf_drv.update_settings(task.input_dict['scf'],
+                                task.input_dict['method_settings'])
+        scf_drv.compute(task.molecule, task.ao_basis, task.min_basis)
+
+        return scf_drv.scf_tensors
+
+    def run_shg(self, inpfile, xcfun_label, ref_result):
+
+        task = MpiTask([inpfile, None])
+        task.input_dict['scf']['checkpoint_file'] = None
+
+        if xcfun_label is not None:
+            task.input_dict['method_settings']['xcfun'] = xcfun_label
+
+        scf_tensors = self.run_scf(task)
+
+        shg_prop = SHG(task.input_dict['response'],
+                       task.input_dict['method_settings'])
+        shg_prop.init_driver(task.mpi_comm, task.ostream)
+        shg_prop.compute(task.molecule, task.ao_basis, scf_tensors)
+        shg_result = shg_prop.rsp_property
+
+        if task.mpi_rank == mpi_master():
+            freq = list(shg_result['beta'].keys())[0]
+
+            for ind, comp in enumerate('xyz'):
+                assert abs(shg_result['beta'][freq][ind].real -
+                           ref_result[comp].real) < 1.0e-6
+                           
+                assert abs(shg_result['beta'][freq][ind].imag -
+                           ref_result[comp].imag) < 1.0e-6
+
+            assert abs(shg_result['beta_bar'][freq].real -
+                       ref_result['beta_bar'].real) < 1.0e-6
+
+            assert abs(shg_result['beta_bar'][freq].imag -
+                       ref_result['beta_bar'].imag) < 1.0e-6
+
+    def test_shg_hf(self):
+
+        here = Path(__file__).parent
+        inpfile = str(here / 'inputs' / 'methanol_shg_reduced.inp')
+
+        xcfun_label = None
+
+        ref_result = {
+            'x':  -40.53256501-11.43101669j,
+            'y':   -0.11772170-0.02074979j,
+            'z':    24.95878533 + 7.63353776j,
+            'beta_bar':  -39.80239953-11.76359430j,
+
+        }
+
+        self.run_shg(inpfile, xcfun_label, ref_result)
+
+    def test_shg_b3lyp(self):
+
+        here = Path(__file__).parent
+        inpfile = str(here / 'inputs' / 'methanol_shg_reduced.inp')
+
+        xcfun_label = 'b3lyp'
+
+        ref_result = {
+            'x': -48.45569661-20.92675811j,
+            'y': -0.09582173-0.00353080j,
+            'z': 32.02927840 + 15.65488519j,
+            'beta_bar': -47.04057086-22.02140860j,
+        }
+
+        self.run_shg(inpfile, xcfun_label, ref_result)
