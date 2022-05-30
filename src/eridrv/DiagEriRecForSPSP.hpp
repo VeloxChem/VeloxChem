1fa3d9c1ed763451f3e1410832fa57d8526913dc
Zilvinas Rinkevicius <rinkevic@kth.se>
2022-05-30 15:31:23 +0200

    * Preparing primary skeleton for autogenerated code.

---
+++ src/eridrv/DiagEriRecForSPSP.hpp
@@ -0,0 +1,13 @@
+//
+//  DiagEriRecForSPSP.hpp
+//  VeloxChem
+//
+//  Created by Zilvinas Rinkevicius on 2022-05-30.
+//
+
+#ifndef DiagEriRecForSPSP_hpp
+#define DiagEriRecForSPSP_hpp
+
+#include <stdio.h>
+
+#endif /* DiagEriRecForSPSP_hpp */
