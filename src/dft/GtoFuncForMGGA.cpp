16b75766ec7b0ba0ce0dd90f7f8e60647b99ec92
Xin Li <lixin.reco@gmail.com>
2022-06-03 21:17:53 +0200

    added GGA molecular gradient for d orbitals

--- src/dft/GtoFuncForMGGA.cpp
+++ src/dft/GtoFuncForMGGA.cpp
@@ -408,45 +408,117 @@ namespace mggarec {  // mggarec namespace
 
         auto fz_xx = cartGtoGridBuffer.data(3);
 
-        auto f0_xy = cartGtoGridBuffer.data(4);
+        auto fxx_xx = cartGtoGridBuffer.data(4);
 
-        auto fx_xy = cartGtoGridBuffer.data(5);
+        auto fxy_xx = cartGtoGridBuffer.data(5);
 
-        auto fy_xy = cartGtoGridBuffer.data(6);
+        auto fxz_xx = cartGtoGridBuffer.data(6);
 
-        auto fz_xy = cartGtoGridBuffer.data(7);
+        auto fyy_xx = cartGtoGridBuffer.data(7);
 
-        auto f0_xz = cartGtoGridBuffer.data(8);
+        auto fyz_xx = cartGtoGridBuffer.data(8);
 
-        auto fx_xz = cartGtoGridBuffer.data(9);
+        auto fzz_xx = cartGtoGridBuffer.data(9);
 
-        auto fy_xz = cartGtoGridBuffer.data(10);
+        auto f0_xy = cartGtoGridBuffer.data(10);
 
-        auto fz_xz = cartGtoGridBuffer.data(11);
+        auto fx_xy = cartGtoGridBuffer.data(11);
 
-        auto f0_yy = cartGtoGridBuffer.data(12);
+        auto fy_xy = cartGtoGridBuffer.data(12);
 
-        auto fx_yy = cartGtoGridBuffer.data(13);
+        auto fz_xy = cartGtoGridBuffer.data(13);
 
-        auto fy_yy = cartGtoGridBuffer.data(14);
+        auto fxx_xy = cartGtoGridBuffer.data(14);
 
-        auto fz_yy = cartGtoGridBuffer.data(15);
+        auto fxy_xy = cartGtoGridBuffer.data(15);
 
-        auto f0_yz = cartGtoGridBuffer.data(16);
+        auto fxz_xy = cartGtoGridBuffer.data(16);
 
-        auto fx_yz = cartGtoGridBuffer.data(17);
+        auto fyy_xy = cartGtoGridBuffer.data(17);
 
-        auto fy_yz = cartGtoGridBuffer.data(18);
+        auto fyz_xy = cartGtoGridBuffer.data(18);
 
-        auto fz_yz = cartGtoGridBuffer.data(19);
+        auto fzz_xy = cartGtoGridBuffer.data(19);
 
-        auto f0_zz = cartGtoGridBuffer.data(20);
+        auto f0_xz = cartGtoGridBuffer.data(20);
 
-        auto fx_zz = cartGtoGridBuffer.data(21);
+        auto fx_xz = cartGtoGridBuffer.data(21);
 
-        auto fy_zz = cartGtoGridBuffer.data(22);
+        auto fy_xz = cartGtoGridBuffer.data(22);
 
-        auto fz_zz = cartGtoGridBuffer.data(23);
+        auto fz_xz = cartGtoGridBuffer.data(23);
+
+        auto fxx_xz = cartGtoGridBuffer.data(24);
+
+        auto fxy_xz = cartGtoGridBuffer.data(25);
+
+        auto fxz_xz = cartGtoGridBuffer.data(26);
+
+        auto fyy_xz = cartGtoGridBuffer.data(27);
+
+        auto fyz_xz = cartGtoGridBuffer.data(28);
+
+        auto fzz_xz = cartGtoGridBuffer.data(29);
+
+        auto f0_yy = cartGtoGridBuffer.data(30);
+
+        auto fx_yy = cartGtoGridBuffer.data(31);
+
+        auto fy_yy = cartGtoGridBuffer.data(32);
+
+        auto fz_yy = cartGtoGridBuffer.data(33);
+
+        auto fxx_yy = cartGtoGridBuffer.data(34);
+
+        auto fxy_yy = cartGtoGridBuffer.data(35);
+
+        auto fxz_yy = cartGtoGridBuffer.data(36);
+
+        auto fyy_yy = cartGtoGridBuffer.data(37);
+
+        auto fyz_yy = cartGtoGridBuffer.data(38);
+
+        auto fzz_yy = cartGtoGridBuffer.data(39);
+
+        auto f0_yz = cartGtoGridBuffer.data(40);
+
+        auto fx_yz = cartGtoGridBuffer.data(41);
+
+        auto fy_yz = cartGtoGridBuffer.data(42);
+
+        auto fz_yz = cartGtoGridBuffer.data(43);
+
+        auto fxx_yz = cartGtoGridBuffer.data(44);
+
+        auto fxy_yz = cartGtoGridBuffer.data(45);
+
+        auto fxz_yz = cartGtoGridBuffer.data(46);
+
+        auto fyy_yz = cartGtoGridBuffer.data(47);
+
+        auto fyz_yz = cartGtoGridBuffer.data(48);
+
+        auto fzz_yz = cartGtoGridBuffer.data(49);
+
+        auto f0_zz = cartGtoGridBuffer.data(50);
+
+        auto fx_zz = cartGtoGridBuffer.data(51);
+
+        auto fy_zz = cartGtoGridBuffer.data(52);
+
+        auto fz_zz = cartGtoGridBuffer.data(53);
+
+        auto fxx_zz = cartGtoGridBuffer.data(54);
+
+        auto fxy_zz = cartGtoGridBuffer.data(55);
+
+        auto fxz_zz = cartGtoGridBuffer.data(56);
+
+        auto fyy_zz = cartGtoGridBuffer.data(57);
+
+        auto fyz_zz = cartGtoGridBuffer.data(58);
+
+        auto fzz_zz = cartGtoGridBuffer.data(59);
 
         // initialize Cartesian buffer to zero
 
@@ -481,6 +553,10 @@ namespace mggarec {  // mggarec namespace
 
                 double fg_0 = -2.0 * bexp;
 
+                double fg_1 = f0_0 * fg_0;
+
+                double fg_2 = fg_1 * fg_0;
+
                 // leading xx component
 
                 double f0_x = dx * f0_0;
@@ -492,6 +568,18 @@ namespace mggarec {  // mggarec namespace
                 fy_xx[j] += f0_x * fg_0 * dx * dy;
 
                 fz_xx[j] += f0_x * fg_0 * dx * dz;
+
+                fxx_xx[j] += fg_2 * dx * dx * dx * dx + 5.0 * fg_1 * dx * dx + 2.0 * f0_0;
+
+                fxy_xx[j] += fg_2 * dx * dx * dx * dy + 2.0 * fg_1 * dx * dy;
+
+                fxz_xx[j] += fg_2 * dx * dx * dx * dz + 2.0 * fg_1 * dx * dz;
+
+                fyy_xx[j] += fg_2 * dx * dx * dy * dy + 1.0 * fg_1 * dx * dx;
+
+                fyz_xx[j] += fg_2 * dx * dx * dy * dz;
+
+                fzz_xx[j] += fg_2 * dx * dx * dz * dz + 1.0 * fg_1 * dx * dx;
 
                 // leading xy component
 
@@ -505,6 +593,18 @@ namespace mggarec {  // mggarec namespace
 
                 fz_xy[j] += f0_x * fg_0 * dy * dz;
 
+                fxx_xy[j] += fg_2 * dx * dx * dx * dy + 3.0 * fg_1 * dx * dy;
+
+                fxy_xy[j] += fg_2 * dx * dx * dy * dy + 1.0 * fg_1 * (dx * dx + dy * dy) + f0_0;
+
+                fxz_xy[j] += fg_2 * dx * dx * dy * dz + 1.0 * fg_1 * dy * dz;
+
+                fyy_xy[j] += fg_2 * dy * dy * dy * dx + 3.0 * fg_1 * dy * dx;
+
+                fyz_xy[j] += fg_2 * dy * dy * dx * dz + 1.0 * fg_1 * dx * dz;
+
+                fzz_xy[j] += fg_2 * dx * dy * dz * dz + 1.0 * fg_1 * dx * dy;
+
                 // leading xz component
 
                 double f0_z = dz * f0_0;
@@ -517,6 +617,18 @@ namespace mggarec {  // mggarec namespace
 
                 fz_xz[j] += f0_x * (1.0 + fg_0 * dz * dz);
 
+                fxx_xz[j] += fg_2 * dx * dx * dx * dz + 3.0 * fg_1 * dx * dz;
+
+                fxy_xz[j] += fg_2 * dx * dx * dz * dy + 1.0 * fg_1 * dz * dy;
+
+                fxz_xz[j] += fg_2 * dx * dx * dz * dz + 1.0 * fg_1 * (dx * dx + dz * dz) + f0_0;
+
+                fyy_xz[j] += fg_2 * dx * dz * dy * dy + 1.0 * fg_1 * dx * dz;
+
+                fyz_xz[j] += fg_2 * dz * dz * dx * dy + 1.0 * fg_1 * dx * dy;
+
+                fzz_xz[j] += fg_2 * dz * dz * dz * dx + 3.0 * fg_1 * dz * dx;
+
                 // leading yy component
 
                 f0_yy[j] += f0_y * dy;
@@ -526,6 +638,18 @@ namespace mggarec {  // mggarec namespace
                 fy_yy[j] += f0_y * (2.0 + fg_0 * dy * dy);
 
                 fz_yy[j] += f0_y * fg_0 * dy * dz;
+
+                fxx_yy[j] += fg_2 * dy * dy * dx * dx + 1.0 * fg_1 * dy * dy;
+
+                fxy_yy[j] += fg_2 * dy * dy * dy * dx + 2.0 * fg_1 * dy * dx;
+
+                fxz_yy[j] += fg_2 * dy * dy * dx * dz;
+
+                fyy_yy[j] += fg_2 * dy * dy * dy * dy + 5.0 * fg_1 * dy * dy + 2.0 * f0_0;
+
+                fyz_yy[j] += fg_2 * dy * dy * dy * dz + 2.0 * fg_1 * dy * dz;
+
+                fzz_yy[j] += fg_2 * dy * dy * dz * dz + 1.0 * fg_1 * dy * dy;
 
                 // leading yz component
 
@@ -537,6 +661,18 @@ namespace mggarec {  // mggarec namespace
 
                 fz_yz[j] += f0_y * (1.0 + fg_0 * dz * dz);
 
+                fxx_yz[j] += fg_2 * dz * dy * dx * dx + 1.0 * fg_1 * dz * dy;
+
+                fxy_yz[j] += fg_2 * dy * dy * dz * dx + 1.0 * fg_1 * dz * dx;
+
+                fxz_yz[j] += fg_2 * dz * dz * dy * dx + 1.0 * fg_1 * dy * dx;
+
+                fyy_yz[j] += fg_2 * dy * dy * dy * dz + 3.0 * fg_1 * dy * dz;
+
+                fyz_yz[j] += fg_2 * dz * dz * dy * dy + 1.0 * fg_1 * (dz * dz + dy * dy) + f0_0;
+
+                fzz_yz[j] += fg_2 * dz * dz * dz * dy + 3.0 * fg_1 * dz * dy;
+
                 // leading zz component
 
                 f0_zz[j] += f0_z * dz;
@@ -546,10 +682,22 @@ namespace mggarec {  // mggarec namespace
                 fy_zz[j] += f0_z * fg_0 * dz * dy;
 
                 fz_zz[j] += f0_z * (2.0 + fg_0 * dz * dz);
+
+                fxx_zz[j] += fg_2 * dz * dz * dx * dx + 1.0 * fg_1 * dz * dz;
+
+                fxy_zz[j] += fg_2 * dz * dz * dy * dx;
+
+                fxz_zz[j] += fg_2 * dz * dz * dz * dx + 2.0 * fg_1 * dz * dx;
+
+                fyy_zz[j] += fg_2 * dz * dz * dy * dy + 1.0 * fg_1 * dz * dz;
+
+                fyz_zz[j] += fg_2 * dz * dz * dz * dy + 2.0 * fg_1 * dz * dy;
+
+                fzz_zz[j] += fg_2 * dz * dz * dz * dz + 5.0 * fg_1 * dz * dz + 2.0 * f0_0;
             }
         }
 
-        genfunc::transform(spherGtoGridBuffer, cartGtoGridBuffer, CSphericalMomentum(2), 0, 0, ngpnts, 4);
+        genfunc::transform(spherGtoGridBuffer, cartGtoGridBuffer, CSphericalMomentum(2), 0, 0, ngpnts, 10);
     }
 
 }  // namespace mggarec
