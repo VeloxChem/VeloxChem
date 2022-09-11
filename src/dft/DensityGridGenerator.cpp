d1c9887eb2ace91cdd068a349e4f71fa02959957
Xin Li <lixin.reco@gmail.com>
2022-09-11 20:22:08 +0200

    test integrateFxcFock for GGA

--- src/dft/DensityGridGenerator.cpp
+++ src/dft/DensityGridGenerator.cpp
@@ -27,6 +27,8 @@
 
 #include <cstring>
 
+#include "omp.h"
+
 #include "DenseLinearAlgebra.hpp"
 
 namespace dengridgen {  // dengridgen namespace
@@ -129,27 +131,44 @@ generateDensityGridForGGA(const int32_t       npoints,
 
     timer.start("Density grid rho");
 
-    for (int32_t nu = 0; nu < gtoValues.getNumberOfRows(); nu++)
+    auto naos = gtoValues.getNumberOfRows();
+
+    auto nthreads = omp_get_max_threads();
+
+    auto F_val = mat_F.values();
+
+    auto chi_val = gtoValues.values();
+
+    auto chi_x_val = gtoValuesX.values();
+
+    auto chi_y_val = gtoValuesY.values();
+
+    auto chi_z_val = gtoValuesZ.values();
+
+    #pragma omp parallel
     {
-        auto F_nu = mat_F.row(nu);
+        auto thread_id = omp_get_thread_num();
 
-        auto chi_nu = gtoValues.row(nu);
+        auto grid_batch_size = mpi::batch_size(npoints, thread_id, nthreads);
 
-        auto chi_x_nu = gtoValuesX.row(nu);
+        auto grid_batch_offset = mpi::batch_offset(npoints, thread_id, nthreads);
 
-        auto chi_y_nu = gtoValuesY.row(nu);
-
-        auto chi_z_nu = gtoValuesZ.row(nu);
-
-        for (int32_t g = 0; g < npoints; g++)
+        for (int32_t nu = 0; nu < naos; nu++)
         {
-            rhoa[g] += F_nu[g] * chi_nu[g];
+            auto nu_offset = nu * npoints;
 
-            gradax[g] += 2.0 * F_nu[g] * chi_x_nu[g];
+            #pragma omp simd aligned(rhoa, gradax, graday, gradaz, F_val, \
+                                     chi_val, chi_x_val, chi_y_val, chi_z_val : VLX_ALIGN)
+            for (int32_t g = grid_batch_offset; g < grid_batch_offset + grid_batch_size; g++)
+            {
+                rhoa[g] += F_val[nu_offset + g] * chi_val[nu_offset + g];
 
-            graday[g] += 2.0 * F_nu[g] * chi_y_nu[g];
+                gradax[g] += 2.0 * F_val[nu_offset + g] * chi_x_val[nu_offset + g];
 
-            gradaz[g] += 2.0 * F_nu[g] * chi_z_nu[g];
+                graday[g] += 2.0 * F_val[nu_offset + g] * chi_y_val[nu_offset + g];
+
+                gradaz[g] += 2.0 * F_val[nu_offset + g] * chi_z_val[nu_offset + g];
+            }
         }
     }
 
