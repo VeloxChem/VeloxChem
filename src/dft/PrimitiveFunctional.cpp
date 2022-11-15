30c7ce72d0bab26b1a4508a1e3fde43407ee8cc1
Roberto Di Remigio Eikås <roberto@totaltrash.xyz>
2022-11-15 14:30:19 +0100

    Finalize XC functional class to wrap LibXC

--- src/dft/PrimitiveFunctional.cpp
+++ src/dft/PrimitiveFunctional.cpp
@@ -24,7 +24,18 @@
 //  along with VeloxChem. If not, see <https://www.gnu.org/licenses/>.
 
 #include "PrimitiveFunctional.hpp"
+
+#include <xc.h>
+
+#include <algorithm>
+#include <cstdint>
+#include <cstring>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
+#include <string>
+
+#include "ErrorHandler.hpp"
 
 CPrimitiveFunctional::CPrimitiveFunctional()
 
@@ -86,7 +97,6 @@ CPrimitiveFunctional::CPrimitiveFunctional(const std::string&                   
 
     , _bThirdOrderFunction(bThirdOrderFunction)
 {
-    
 }
 
 CPrimitiveFunctional::CPrimitiveFunctional(const CPrimitiveFunctional& source)
@@ -113,7 +123,6 @@ CPrimitiveFunctional::CPrimitiveFunctional(const CPrimitiveFunctional& source)
 
     , _bThirdOrderFunction(source._bThirdOrderFunction)
 {
-    
 }
 
 CPrimitiveFunctional::CPrimitiveFunctional(CPrimitiveFunctional&& source) noexcept
@@ -140,7 +149,6 @@ CPrimitiveFunctional::CPrimitiveFunctional(CPrimitiveFunctional&& source) noexce
 
     , _bThirdOrderFunction(std::move(source._bThirdOrderFunction))
 {
-    
 }
 
 CPrimitiveFunctional::~CPrimitiveFunctional()
@@ -155,23 +163,23 @@ CPrimitiveFunctional::operator=(const CPrimitiveFunctional& source)
     _label = source._label;
 
     _xcFuncType = source._xcFuncType;
-    
+
     _abFirstOrderFunction = source._abFirstOrderFunction;
-    
+
     _aFirstOrderFunction = source._aFirstOrderFunction;
-    
+
     _bFirstOrderFunction = source._bFirstOrderFunction;
-    
+
     _abSecondOrderFunction = source._abSecondOrderFunction;
-    
+
     _aSecondOrderFunction = source._aSecondOrderFunction;
-    
+
     _bSecondOrderFunction = source._bSecondOrderFunction;
 
     _abThirdOrderFunction = source._abThirdOrderFunction;
-    
+
     _aThirdOrderFunction = source._aThirdOrderFunction;
-    
+
     _bThirdOrderFunction = source._bThirdOrderFunction;
 
     return *this;
@@ -185,23 +193,23 @@ CPrimitiveFunctional::operator=(CPrimitiveFunctional&& source) noexcept
     _label = std::move(source._label);
 
     _xcFuncType = std::move(source._xcFuncType);
-    
+
     _abFirstOrderFunction = std::move(source._abFirstOrderFunction);
-    
+
     _aFirstOrderFunction = std::move(source._aFirstOrderFunction);
-    
+
     _bFirstOrderFunction = std::move(source._bFirstOrderFunction);
-    
+
     _abSecondOrderFunction = std::move(source._abSecondOrderFunction);
-    
+
     _aSecondOrderFunction = std::move(source._aSecondOrderFunction);
-    
+
     _bSecondOrderFunction = std::move(source._bSecondOrderFunction);
 
     _abThirdOrderFunction = std::move(source._abThirdOrderFunction);
-    
+
     _aThirdOrderFunction = std::move(source._aThirdOrderFunction);
-    
+
     _bThirdOrderFunction = std::move(source._bThirdOrderFunction);
 
     return *this;
@@ -213,7 +221,7 @@ CPrimitiveFunctional::operator==(const CPrimitiveFunctional& other) const
     if (this == &other) return true;
 
     if (_label != other._label) return false;
-    
+
     if (_xcFuncType != other._xcFuncType) return false;
 
     return true;
@@ -226,39 +234,32 @@ CPrimitiveFunctional::operator!=(const CPrimitiveFunctional& other) const
 }
 
 void
-CPrimitiveFunctional::compute(      CXCGradientGrid& xcGradientGrid,
-                              const double           factor,
-                              const CDensityGrid&    densityGrid) const
+CPrimitiveFunctional::compute(CXCGradientGrid& xcGradientGrid, const double factor, const CDensityGrid& densityGrid) const
 {
     if (densityGrid.getDensityGridType() == dengrid::ab) _abFirstOrderFunction(xcGradientGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::lima) _aFirstOrderFunction(xcGradientGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::limb) _bFirstOrderFunction(xcGradientGrid, factor, densityGrid);
 }
 
 void
-CPrimitiveFunctional::compute(      CXCHessianGrid& xcHessianGrid,
-                              const double          factor,
-                              const CDensityGrid&   densityGrid) const
+CPrimitiveFunctional::compute(CXCHessianGrid& xcHessianGrid, const double factor, const CDensityGrid& densityGrid) const
 {
     if (densityGrid.getDensityGridType() == dengrid::ab) _abSecondOrderFunction(xcHessianGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::lima) _aSecondOrderFunction(xcHessianGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::limb) _bSecondOrderFunction(xcHessianGrid, factor, densityGrid);
 }
 
 void
-CPrimitiveFunctional::compute(      CXCCubicHessianGrid& xcCubicHessianGrid,
-                              const double          factor,
-                              const CDensityGrid&   densityGrid) const
+CPrimitiveFunctional::compute(CXCCubicHessianGrid& xcCubicHessianGrid, const double factor, const CDensityGrid& densityGrid) const
 {
-
     if (densityGrid.getDensityGridType() == dengrid::ab) _abThirdOrderFunction(xcCubicHessianGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::lima) _aThirdOrderFunction(xcCubicHessianGrid, factor, densityGrid);
-    
+
     if (densityGrid.getDensityGridType() == dengrid::limb) _bThirdOrderFunction(xcCubicHessianGrid, factor, densityGrid);
 }
 
@@ -274,7 +275,6 @@ CPrimitiveFunctional::getFunctionalType() const
     return _xcFuncType;
 }
 
-
 std::ostream&
 operator<<(std::ostream& output, const CPrimitiveFunctional& source)
 {
@@ -283,14 +283,352 @@ operator<<(std::ostream& output, const CPrimitiveFunctional& source)
     output << "[CPrimitiveFunctional (Object):" << &source << "]" << std::endl;
 
     output << "_label: " << source._label << std::endl;
-    
+
     output << "_xcFuncType:" << to_string(source._xcFuncType);
 
     output << "_abFirstOrderFunction: " << &(source._abFirstOrderFunction) << std::endl;
-    
+
     output << "_aFirstOrderFunction: " << &(source._aFirstOrderFunction) << std::endl;
-    
+
     output << "_bFirstOrderFunction: " << &(source._bFirstOrderFunction) << std::endl;
 
     return output;
 }
+
+Functional::Functional(const std::vector<std::string>& labels, const std::vector<double>& coeffs, bool polarized)
+{
+    errors::assertMsgCritical(labels.size() == coeffs.size(),
+                              std::string(__func__) +
+                                  ": exchange-correlation functional labels and mixing coefficients must have the same size! labels.size() = " +
+                                  std::to_string(labels.size()) + ", coeffs.size() = " + std::to_string(coeffs.size()));
+
+    auto is_in_family = [](const auto& options, auto family) {
+        return std::any_of(options.begin(), options.end(), [family](auto a) { return a == family; });
+    };
+
+    auto nspin = (_polarized ? 2 : 1);
+
+    auto total_x = 0.0;
+
+    auto total_c = 0.0;
+
+    std::vector<Component> x_funcs, c_funcs;
+
+    for (auto i = 0; i < labels.size(); ++i)
+    {
+        auto label = labels[i];
+        auto coeff = coeffs[i];
+
+        auto funcID = xc_functional_get_number(label.c_str());
+
+        xc_func_type func;
+        if (xc_func_init(&func, funcID, nspin))
+        {
+            errors::msgCritical(std::string("Could not find required LibXC functional ") + label);
+        }
+
+        auto kind = func.info->kind;
+
+        if (kind == XC_EXCHANGE)
+        {
+            total_x += coeff;
+            x_funcs.push_back({coeff, func});
+        }
+        else if (kind == XC_CORRELATION)
+        {
+            total_c += coeff;
+            c_funcs.push_back({coeff, func});
+        }
+        else
+        {
+            // clean up
+            xc_func_end(&func);
+
+            // error out if the kind is XC_EXCHANGE_CORRELATION ("canned" x-c mix in LibXC) or XC_KINETIC
+            errors::msgCritical("We cannot handle functionals of kind XC_EXCHANGE_CORRELATION or XC_KINETIC");
+        }
+
+        auto flags = func.info->flags;
+
+        // which derivative orders do we have for this x-c mixture?
+        _hasExc = _hasExc && (flags & XC_FLAGS_HAVE_EXC);
+        _hasVxc = _hasVxc && (flags & XC_FLAGS_HAVE_VXC);
+        _hasFxc = _hasFxc && (flags & XC_FLAGS_HAVE_FXC);
+        _hasKxc = _hasKxc && (flags & XC_FLAGS_HAVE_KXC);
+        _hasLxc = _hasLxc && (flags & XC_FLAGS_HAVE_LXC);
+
+        // which family does this x-c mixture belong to?
+        auto family = func.info->family;
+
+        // GGA?
+        if (is_in_family(std::array{XC_FAMILY_GGA, XC_FAMILY_HYB_GGA}, family))
+        {
+            _isGGA = true;
+        }
+
+        // metaGGA?
+        if (is_in_family(std::array{XC_FAMILY_MGGA, XC_FAMILY_HYB_MGGA}, family))
+        {
+            _isGGA     = true;
+            _isMetaGGA = true;
+        }
+
+        // hybrid?
+        // FIXME I believe these checks are only relevant if we allow usage of any of the "prebaked" mixes in LibXC
+        if (is_in_family(std::array{XC_FAMILY_HYB_LDA, XC_FAMILY_HYB_GGA, XC_FAMILY_HYB_MGGA}, family))
+        {
+            // range separation using the error function or the Yukawa function
+            if ((flags & XC_FLAGS_HYB_CAM) || (flags & XC_FLAGS_HYB_CAMY))
+            {
+                _isRangeSeparatedHybrid = true;
+            }
+
+            // global hybrid
+            if (!_isRangeSeparatedHybrid)
+            {
+                _isGlobalHybrid = true;
+            }
+        }
+    }
+
+    _numberOfExchangeFunctionals    = x_funcs.size();
+    _numberOfCorrelationFunctionals = c_funcs.size();
+    // join the two lists together: first all exchange, then all correlation functionals.
+    _components.reserve(_numberOfExchangeFunctionals + _numberOfCorrelationFunctionals);
+    _components.insert(_components.end(), x_funcs.cbegin(), x_funcs.cend());
+    _components.insert(_components.end(), c_funcs.cbegin(), c_funcs.cend());
+
+    // warn if the sum of X coefficients and C coefficients is greater than 1
+    errors::assertMsgWarning(total_x <= 1.0, "Sum of coefficients for exchange functionals is greater than 1. We hope you know what your are doing!");
+    errors::assertMsgWarning(total_c <= 1.0,
+                             "Sum of coefficients for correlation functionals is greater than 1. We hope you know what your are doing!");
+
+    _xcName   = detail::getXCName(x_funcs, c_funcs);
+    _citation = detail::getCitation(x_funcs, c_funcs);
+}
+
+Functional::~Functional()
+{
+    std::for_each(std::begin(_components), std::end(_components), [](Component& c) { xc_func_end(&std::get<1>(c)); });
+    _components.clear();
+}
+
+auto
+Functional::repr() const -> std::string
+{
+    std::ostringstream os;
+
+    os << std::endl;
+
+    os << "[Functional (Object):" << this << "]" << std::endl;
+
+    os << "Functional name: " << getXCName() << std::endl;
+
+    os << "Functional family: ";
+    if (_isMetaGGA)
+    {
+        os << "metaGGA." << std::endl;
+    }
+    else if (_isGGA)
+    {
+        os << "GGA." << std::endl;
+    }
+    else
+    {
+        os << "LDA." << std::endl;
+    }
+    os << std::endl;
+
+    os << "Available derivatives up to and including ";
+    if (_hasLxc)
+    {
+        os << "4th order." << std::endl;
+    }
+    else if (_hasKxc)
+    {
+        os << "3rd order." << std::endl;
+    }
+    else if (_hasFxc)
+    {
+        os << "2nd order." << std::endl;
+    }
+    else if (_hasVxc)
+    {
+        os << "1st order." << std::endl;
+    }
+    else
+    {
+        os << "0th order." << std::endl;
+    }
+
+    os << std::endl;
+
+    os << getCitation();
+
+    return os.str();
+}
+
+auto
+Functional::compute(CXCGradientGrid& xcGradientGrid, const CDensityGrid& densityGrid) const -> void
+{
+    xcGradientGrid.zero();
+
+    auto np = densityGrid.getNumberOfGridPoints();
+
+    auto rhoa = densityGrid.alphaDensity(0);
+
+    auto rhob = densityGrid.betaDensity(0);
+
+    auto rho_scratch = mem::malloc<double>(_polarized ? 2 * np : np);
+
+    if (_polarized)
+    {
+#pragma omp simd aligned(rho_scratch, rhoa, rhob : VLX_ALIGN)
+        for (auto i = 0; i < np; ++i)
+        {
+            rho_scratch[2 * i]     = rhoa[i];
+            rho_scratch[2 * i + 1] = rhob[i];
+        }
+    }
+    else
+    {
+#pragma omp simd aligned(rho_scratch, rhoa, rhob : VLX_ALIGN)
+        for (auto i = 0; i < np; ++i)
+        {
+            rho_scratch[i] = rhoa[i] + rhob[i];
+        }
+    }
+
+    auto fexc = xcGradientGrid.xcFunctionalValues();
+
+    auto grhoa = xcGradientGrid.xcGradientValues(xcvars::rhoa);
+
+    auto grhob = xcGradientGrid.xcGradientValues(xcvars::rhob);
+
+    auto vxc_scratch = mem::malloc<double>(_polarized ? 2 * np : np);
+
+    for (const auto& [coeff, func] : _components)
+    {
+        xc_lda_exc_vxc(&func, np, rhoa, fexc, vxc_scratch);
+
+        auto c = coeff;
+
+// FIXME only valid for unpolarized at the moment
+#pragma omp simd aligned(vxc_scratch, grhoa, grhob : VLX_ALIGN)
+        for (auto i = 0; i < np; ++i)
+        {
+            grhoa[i] += c * vxc_scratch[i];
+            grhob[i] += c * vxc_scratch[i];
+        }
+    }
+
+    mem::free(vxc_scratch);
+
+    mem::free(rho_scratch);
+}
+
+std::ostream&
+operator<<(std::ostream& output, const Functional& source)
+{
+    return (output << source.repr());
+}
+
+auto
+getLibXCDescription() -> std::string
+{
+    std::ostringstream os;
+
+    os << "LibXC version " << std::string(xc_version_string()) << std::endl;
+
+    os << xc_reference() << " (" << xc_reference_doi() << ")";
+
+    return os.str();
+}
+
+namespace detail {
+auto
+getXCName(const std::vector<Component>& x_funcs, const std::vector<Component>& c_funcs) -> std::string
+{
+    // create label for this mix of functionals
+    std::ostringstream os;
+
+    auto i = 1;
+    for (const auto& [coeff, func] : x_funcs)
+    {
+        auto name = std::string(xc_functional_get_name(func.info->number));
+
+        std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return std::toupper(c); });
+
+        os << std::setprecision(4) << coeff << "*" << name << (i != x_funcs.size() ? " + " : "");
+        ++i;
+    }
+
+    os << ", ";
+
+    i = 1;
+    for (const auto& [coeff, func] : c_funcs)
+    {
+        auto name = std::string(xc_functional_get_name(func.info->number));
+
+        std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return std::toupper(c); });
+
+        os << std::setprecision(4) << coeff << "*" << name << (i != c_funcs.size() ? " + " : "");
+        ++i;
+    }
+
+    return os.str();
+}
+
+auto
+getFunctionalCitation(const xc_func_type& func) -> std::string
+{
+    std::ostringstream os;
+
+    for (auto i = 0; i < XC_MAX_REFERENCES; ++i)
+    {
+        if (func.info->refs[i])
+        {
+            if (i != 0)
+            {
+                os << std::endl;
+            }
+            os << "    ";
+            os << func.info->refs[i]->ref;
+            if (std::strlen(func.info->refs[i]->doi) > 0)
+            {
+                os << " (";
+                os << func.info->refs[i]->doi;
+                os << ")";
+            }
+        }
+    }
+
+    return os.str();
+}
+
+auto
+getCitation(const std::vector<Component>& x_funcs, const std::vector<Component>& c_funcs) -> std::string
+{
+    std::ostringstream os;
+
+    os << "Exchange components: " << std::endl;
+
+    for (const auto& [_, func] : x_funcs)
+    {
+        os << " * " << std::string(func.info->name) << std::endl;
+        os << detail::getFunctionalCitation(func) << std::endl;
+    }
+
+    os << std::endl;
+
+    os << "Correlation components: " << std::endl;
+
+    for (const auto& [_, func] : c_funcs)
+    {
+        os << " * " << std::string(func.info->name) << std::endl;
+        os << detail::getFunctionalCitation(func) << std::endl;
+    }
+
+    return os.str();
+}
+}  // namespace detail
--- src/dft/PrimitiveFunctional.hpp
+++ src/dft/PrimitiveFunctional.hpp
@@ -30,6 +30,8 @@
 #include <functional>
 #include <ostream>
 #include <string>
+#include <tuple>
+#include <vector>
 
 #include "DensityGrid.hpp"
 #include "XCCubicHessianGrid.hpp"
@@ -48,7 +50,7 @@ using def_vxc3_func_typ = void(CXCCubicHessianGrid&, const double factor, const 
 /**
  Class CPrimitiveFunctional stores information about primitive exchange-correlation functional
  and provides methods to perform actions with stored exchange-correlation functional.
- 
+
  @author Z. Rinkevicius
  */
 class CPrimitiveFunctional
@@ -249,5 +251,169 @@ class CPrimitiveFunctional
      */
     friend std::ostream& operator<<(std::ostream& output, const CPrimitiveFunctional& source);
 };
+
+// Forward-declaration from LibXC
+struct xc_func_type;
+
+using Component = std::tuple<double, xc_func_type>;
+
+/**
+ * Class CPrimitiveFunctional is a wrapper to the C functions and structs provided by LibXC.
+ *
+ * @author R. Di Remigio Eikås, Z. Rinkevicius
+ */
+class Functional
+{
+   private:
+    /** Whether the functional is spin-polarized or not. */
+    bool _polarized{false};
+
+    /** @{ Exchange-correlation family. */
+    /** Whether the functional is of generalized-gradient approximation type. */
+    bool _isGGA{false};
+
+    /** Whether the functional is of meta-generalized-gradient approximation type. */
+    bool _isMetaGGA{false};
+
+    /** Whether the functional is of global hybrid type. */
+    bool _isGlobalHybrid{false};
+
+    /** Whether the functional is of range-separated hybrid type. */
+    bool _isRangeSeparatedHybrid{false};
+    /** }@ */
+
+    /** @{ Available derivative orders. */
+    /** Whether has zeroth-order derivatives available. */
+    bool _hasExc{true};
+
+    /** Whether has first-order derivatives available. */
+    bool _hasVxc{true};
+
+    /** Whether has second-order derivatives available. */
+    bool _hasFxc{true};
+
+    /** Whether has third-order derivatives available. */
+    bool _hasKxc{true};
+
+    /** Whether has fourth-order derivatives available. */
+    bool _hasLxc{true};
+    /** }@ */
+
+    int32_t _numberOfExchangeFunctionals{0};
+    int32_t _numberOfCorrelationFunctionals{0};
+
+    std::string _xcName{""};
+
+    std::string _citation{""};
+
+    /** The primitive exchange-correlation functionals and their coefficients. */
+    std::vector<Component> _components;
+
+   public:
+    /** Creates an exchange-correlation functional object.
+     *
+     * @param[in] labels list of labels of component exchange and correlation functionals.
+     * @param[in] coeffs list of coefficients for the components of the functional.
+     * @param[in] polarized whether the functional is spin-polarized.
+     */
+    Functional(const std::vector<std::string>& labels, const std::vector<double>& coeffs, bool polarized);
+
+    /** Copy-constructor.
+     *
+     * @param[in] src the exchange-correlation functional.
+     */
+    Functional(const Functional& src);
+
+    /** Move-constructor.
+     *
+     *  @param[in] src the exchange-correlation functional object.
+     */
+    Functional(Functional&& src) noexcept;
+
+    /** Destroys an exchange-correlation functional object. */
+    ~Functional();
+
+    /** Copy-assignment operator.
+     *
+     * @param[in] src the exchange-correlation functional object.
+     */
+    auto operator=(const Functional& src) -> Functional&;
+
+    /** Move-assignment operator.
+     *
+     * @param[in] src the exchange-correlation functional object.
+     */
+    auto operator=(Functional&& src) noexcept -> Functional&;
+
+    /** Equality operator.
+     *
+     * @param[in] other the exchange-correlation functional object.
+     * @return true if exchange-correlation functional objects are equal, false otherwise.
+     */
+    auto operator==(const Functional& other) const -> bool;
+
+    /** Inequality operator.
+     *
+     * @param[in] other the exchange-correlation functional object.
+     * @return true if exchange-correlation functional objects are different, false otherwise.
+     */
+    auto operator!=(const Functional& other) const -> bool;
+
+    /** Gets label of primitive exchange-correlation functional.
+     *
+     * @return the label.
+     */
+    auto getXCName() const -> std::string { return _xcName; }
+
+    /** Get bibliographic reference of primitive functional.
+     *
+     * @return the reference.
+     */
+    auto getCitation() const -> std::string { return _citation; }
+
+    auto getNumberOfExchangeFunctionals() const -> int32_t { return _numberOfExchangeFunctionals; }
+
+    auto getNumberOfCorrelationFunctionals() const -> int32_t { return _numberOfCorrelationFunctionals; }
+
+    /** String representation of primitive functional. */
+    auto repr() const -> std::string;
+
+    /** Computes first derivative of exchange-correlation functional for given density grid.
+     *
+     * @param[in,out] xcGradientGrid the exchange-correlation gradient grid object.
+     * @param[in] densityGrid the density grid object.
+     */
+    auto compute(CXCGradientGrid& xcGradientGrid, const CDensityGrid& densityGrid) const -> void;
+
+    /** Computes second derivative of exchange-correlation functional for given density grid.
+     *
+     * @param[in,out] xcHessianGrid the exchange-correlation hessian grid object.
+     * @param[in] densityGrid the density grid object.
+     */
+    auto compute(CXCHessianGrid& xcHessianGrid, const CDensityGrid& densityGrid) const -> void;
+
+    /** Computes third derivative of exchange-correlation functional for given density grid.
+     *
+     * @param[in,out] xcCubicHessianGrid the exchange-correlation third-derivative grid object.
+     * @param[in] densityGrid the density grid object.
+     */
+    auto compute(CXCCubicHessianGrid& xcCubicHessianGrid, const CDensityGrid& densityGrid) const -> void;
+};
+
+std::ostream& operator<<(std::ostream& output, const Functional& source);
+
+/** Get descriptiton, with bibliographic reference, LibXC in use.
+ *
+ * @return the description.
+ */
+auto getLibXCDescription() -> std::string;
+
+namespace detail {
+auto getXCName(const std::vector<Component>& x_funcs, const std::vector<Component>& c_funcs) -> std::string;
+
+auto getFunctionalCitation(const xc_func_type& func) -> std::string;
+
+auto getCitation(const std::vector<Component>& x_funcs, const std::vector<Component>& c_funcs) -> std::string;
+}  // namespace detail
 
 #endif /* PrimitiveFunctional_hpp */
