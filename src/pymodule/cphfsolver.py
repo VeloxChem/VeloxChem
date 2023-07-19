fixed printout, set subspace solver and analytical gradient as default

src/pymodule/cphfsolver.py
@@ -86,7 +86,7 @@ class CphfSolver(LinearSolver):
 
         super().__init__(comm, ostream)
 
-        self.use_subspace_solver = False
+        self.use_subspace_solver = True
         self.print_residuals = False
         self.max_iter = 25
 
@@ -142,6 +142,11 @@ class CphfSolver(LinearSolver):
             and an auxiliary Fock matrix (oo block of the CPHF coefficients
             contracted with the two-electron integrals).
         """
+
+        if self.norm_thresh is None:
+            self.norm_thresh = self.conv_thresh * 1.0e-6
+        if self.lindep_thresh is None:
+            self.lindep_thresh = self.conv_thresh * 1.0e-2
 
         # sanity check
         nalpha = molecule.number_of_alpha_electrons()
@@ -571,23 +576,6 @@ class CphfSolver(LinearSolver):
             dist_trials_proj = old_trials.matmul_AB_no_gather(bT_new_trials)
             dist_trials.data -= dist_trials_proj.data
 
-            # TODO: remove commented out code!!
-            ### b b.T
-            ###bT_new_trials = np.matmul(old_trials.data, old_trials.data.T)
-            ##bT_new = np.matmul(old_trials.data.T, dist_trials.data)
-            ### (b b.T) t
-            ###dist_trials_proj = np.matmul(bT_new_trials, dist_trials.data)
-
-            ###print("Shapes: old trials, new trials      b b.T       b.T n\n", old_trials.data.shape, dist_trials.data.shape, bT_new_trials.shape, bT_new.shape)
-            ##dist_trials_proj_proper = np.matmul(old_trials.data, bT_new)
-
-            ###print("\nHERE\n", dist_trials_proj-dist_trials_proj_proper)
-            ###print()
-            ###print(dist_trials_proj_proper)
-            ### t = t - (b b.T) t
-            #dist_trials.data -= dist_trials_proj_proper
-
-
         # remove linear dependencies and orthonormalize trial vectors
         if renormalize:
             if dist_trials.data.ndim > 0 and dist_trials.shape(0) > 0:
@@ -914,7 +902,7 @@ class CphfSolver(LinearSolver):
     def compute_rhs(self, molecule, basis, scf_tensors):
         """
         Computes the right hand side for the CPHF equations for
-        all atomic coordinates.
+        the analytical Hessian, all atomic coordinates.
 
         :param molecule:
             The molecule.
@@ -927,6 +915,7 @@ class CphfSolver(LinearSolver):
             The RHS of the CPHF equations.
         """
 
+        t1 = tm.time()
         if self.rank == mpi_master():
             density = scf_tensors['D_alpha']
             #overlap = self.scf_drv.scf_tensors['S']
@@ -1007,8 +996,13 @@ class CphfSolver(LinearSolver):
 
         self._comp_lr_fock(fock_uij, ao_density_uij, molecule,
                           basis, eri_dict, dft_dict, pe_dict, self.profiler)
-
+        t2 = tm.time()
         if self.rank == mpi_master():
+            self.ostream.print_info('CPHF/CPKS RHS computed in' +
+                                     ' {:.2f} sec.'.format(t2 - t1))
+            self.ostream.print_blank()
+            self.ostream.flush()
+
             # TODO: how can this be done better?
             fock_uij_numpy = np.zeros((natm,3,nao,nao))
             for i in range(natm):

src/pymodule/scfhessiandriver.py
@@ -375,6 +375,7 @@ class ScfHessianDriver(HessianDriver):
                             'ScfHessianDriver: SCF did not converge')
         scf_drv.ostream.unmute()
 
+    # TODO: replace all einsum with multidot!
     def compute_analytical(self, molecule, ao_basis, scf_drv, profiler):
         """
         Computes the analytical nuclear Hessian.
@@ -424,7 +425,7 @@ class ScfHessianDriver(HessianDriver):
                               + np.einsum('mi,xyia,na->xymn',
                                         mo_occ, cphf_ov, mo_vir)
                             )
-
+        t1 = tm.time()
         # Parts related to first-order integral derivatives
         if self.pople_hessian:
             fock_uij = cphf_solution_dict['fock_uij']
@@ -467,6 +468,13 @@ class ScfHessianDriver(HessianDriver):
                                                 scf_drv.xcfun.get_func_label())
             scf_drv.comm.reduce(hessian_dft_xc, root=mpi_master())
 
+        t2 = tm.time()
+        if self.rank == mpi_master():
+            self.ostream.print_info('First order derivative contributions'
+                                    + ' to the Hessian computed in' +
+                                     ' {:.2f} sec.'.format(t2 - t1))
+            self.ostream.print_blank()
+            self.ostream.flush()
         # Parts related to second-order integral derivatives
         hessian_2nd_order_derivatives = np.zeros((natm, natm, 3, 3))
         for i in range(natm):
@@ -514,6 +522,13 @@ class ScfHessianDriver(HessianDriver):
         if self.dft:
             self.hessian += hessian_dft_xc
 
+        t3 = tm.time()
+        if self.rank == mpi_master():
+            self.ostream.print_info('Second order derivative contributions'
+                                    + ' to the Hessian computed in' +
+                                     ' {:.2f} sec.'.format(t3 - t2))
+            self.ostream.print_blank()
+            self.ostream.flush()
         # Calculate the gradient of the dipole moment, needed for IR intensities
         self.compute_dipole_gradient(molecule, ao_basis, scf_drv,
                                     perturbed_density)

src/pymodule/tddftgradientdriver.py
@@ -187,8 +187,7 @@ class TddftGradientDriver(GradientDriver):
         # the first state only.
         if self.numerical:
             scf_drv.ostream.mute()
-            self.compute_numerical(molecule, basis, scf_drv, rsp_drv, 
-                                   self.state_deriv_index[0]-1)
+            self.compute_numerical(molecule, basis, scf_drv, rsp_drv, rsp_results)
             scf_drv.ostream.unmute()
         else:
             self.compute_analytical(molecule, basis, scf_drv, rsp_results)
@@ -207,6 +206,7 @@ class TddftGradientDriver(GradientDriver):
             self.ostream.print_blank()
             self.ostream.flush()
 
+    # TODO: replace all np.einsum with multidot!
     def compute_analytical(self, molecule, basis, scf_drv, rsp_results):
         """
         Performs calculation of analytical gradient.
@@ -271,14 +271,26 @@ class TddftGradientDriver(GradientDriver):
         dof = self.comm.bcast(dof, root=mpi_master())
 
         # ground state gradient
+        t1 = tm.time()
         gs_grad_drv = ScfGradientDriver()
         gs_grad_drv.update_settings(self.grad_dict, self.method_dict)
 
         gs_grad_drv.ostream.mute()
         gs_grad_drv.compute(molecule, basis, scf_drv)
         gs_grad_drv.ostream.unmute()
+        t2 = tm.time()
 
         if self.rank == mpi_master():
+            if gs_grad_drv.numerical:
+                gs_gradient_type = "Numerical GS gradient"
+            else:
+                gs_gradient_type = "Analytical GS gradient"
+            self.ostream.print_info(gs_gradient_type
+                                    + ' computed in'
+                                    + ' {:.2f} sec.'.format(t2 - t1))
+            self.ostream.print_blank()
+            self.ostream.flush()
+
             self.gradient = np.zeros((dof, natm, 3))
             self.gradient += gs_grad_drv.get_gradient()
 
@@ -291,6 +303,8 @@ class TddftGradientDriver(GradientDriver):
                 d_hcore = hcore_deriv(molecule, basis, i)
                 d_eri = eri_deriv(molecule, basis, i)
 
+                #for x in range(dof):
+                #    self.gradient[s,x,i] += np.sum(np.matmul(relaxed_density_ao[
                 self.gradient[:,i] += 1.0 * np.einsum('smn,xmn->sx',
                                                     relaxed_density_ao,
                                                     d_hcore)
@@ -360,8 +374,7 @@ class TddftGradientDriver(GradientDriver):
                 if self.rank == mpi_master():
                     self.gradient[s] += tddft_xcgrad
 
-    def compute_energy(self, molecule, basis, scf_drv, rsp_drv,
-                       state_deriv_index=0):
+    def compute_energy(self, molecule, basis, scf_drv, rsp_drv, rsp_results):
         """
         Computes the energy at the current position.
 
@@ -377,6 +390,12 @@ class TddftGradientDriver(GradientDriver):
             The index of the excited state of interest.
         """
 
+        if isinstance(self.state_deriv_index, int):
+            # Python numbering starts at 0
+            print("!!!!! Int!")
+            state_deriv_index = self.state_deriv_index - 1
+        else:
+            state_deriv_index = self.state_deriv_index[0] - 1
         scf_drv.restart = False
         scf_results = scf_drv.compute(molecule, basis)
         assert_msg_critical(scf_drv.is_converged,