added timing printout for gradient and hessian, fixed gradient python test

src/pymodule/scfgradientdriver.py
@@ -63,6 +63,9 @@ class ScfGradientDriver(GradientDriver):
 
         self.flag = 'SCF Gradient Driver'
 
+        # for numerical gradient
+        self.delta_h = 0.001
+
 
     def compute(self, molecule, ao_basis, scf_drv):
         """
@@ -110,8 +113,8 @@ class ScfGradientDriver(GradientDriver):
             The molecule.
         :param ao_basis:
             The AO basis set.
-		:param scf_drv:
-			The SCF driver.
+        :param scf_drv:
+            The SCF driver.
         """
 
         natm = molecule.number_of_atoms()
@@ -130,14 +133,24 @@ class ScfGradientDriver(GradientDriver):
             epsilon_dm_ao = -np.linalg.multi_dot([mo_occ, eo_diag, mo_occ.T])
 
             for i in range(natm):
+                t0 = tm.time()
                 d_ovlp = overlap_deriv(molecule, ao_basis, i)
                 d_hcore = hcore_deriv(molecule, ao_basis, i)
                 d_eri = eri_deriv(molecule, ao_basis, i)
+                t1 = tm.time()
+                self.ostream.print_info("Import of integral derivatives"
+                                    + ' took'
+                                    + ' {:.2f} sec.'.format(t1 - t0))
+                self.ostream.flush()
 
                 self.gradient[i] = (
                     2.0 * np.einsum('mn,xmn->x', one_pdm_ao, d_hcore) +
                     2.0 * np.einsum('mn,xmn->x', epsilon_dm_ao, d_ovlp))
-
+                t2 = tm.time()
+                self.ostream.print_info("Core Hamiltonian and overlap"
+                                    + ' derivative contribution computed in'
+                                    + ' {:.2f} sec.'.format(t2 - t1))
+                self.ostream.flush()
                 if self.dft:
                     if self.xcfun.is_hybrid():
                         frac_K = self.xcfun.get_frac_exact_exchange()
@@ -150,7 +163,12 @@ class ScfGradientDriver(GradientDriver):
                     'mt,np,xmtnp->x', one_pdm_ao, one_pdm_ao, d_eri)
                 self.gradient[i] -= frac_K * np.einsum(
                     'mt,np,xmnpt->x', one_pdm_ao, one_pdm_ao, d_eri)
-
+                t3 = tm.time()
+                self.ostream.print_info("Two electron integral derivatives"
+                                    + ' contribution computed in'
+                                    + ' {:.2f} sec.'.format(t3 - t2))
+                self.ostream.print_blank()
+                self.ostream.flush()
         # Add the xc contribution
         if self.dft:
             density = scf_drv.density

src/pymodule/tddftgradientdriver.py
@@ -298,6 +298,7 @@ class TddftGradientDriver(GradientDriver):
             # with density matrices
             # add the corresponding contribution to the gradient
             for i in range(natm):
+                t0 = tm.time()
                 # taking integral derivatives from pyscf
                 d_ovlp = overlap_deriv(molecule, basis, i)
                 d_hcore = hcore_deriv(molecule, basis, i)
@@ -305,12 +306,26 @@ class TddftGradientDriver(GradientDriver):
 
                 #for x in range(dof):
                 #    self.gradient[s,x,i] += np.sum(np.matmul(relaxed_density_ao[
+                t2 = tm.time()
+                self.ostream.print_info("Import of integral derivatives"
+                                    + ' took'
+                                    + ' {:.2f} sec.'.format(t2 - t0))
+                self.ostream.flush()
                 self.gradient[:,i] += 1.0 * np.einsum('smn,xmn->sx',
                                                     relaxed_density_ao,
                                                     d_hcore)
+                t3 = tm.time()
+                self.ostream.print_info("Core Hamiltonian derivative"
+                                    + ' contribution computed in'
+                                    + ' {:.2f} sec.'.format(t3 - t2))
+                self.ostream.flush()
                 self.gradient[:,i] += 1.0 * np.einsum('smn,xmn->sx',
                                                        2.0 * omega_ao, d_ovlp)
-
+                t4 = tm.time()
+                self.ostream.print_info("Overlap derivative contribution "
+                                    + ' computed in'
+                                    + ' {:.2f} sec.'.format(t4 - t3))
+                self.ostream.flush()
                 if self.dft:
                     if self.xcfun.is_hybrid():
                         frac_K = self.xcfun.get_frac_exact_exchange()
@@ -341,6 +356,12 @@ class TddftGradientDriver(GradientDriver):
                                 'smn,spt,xtnmp->sx', x_minus_y_ao,
                                 x_minus_y_ao + x_minus_y_ao.transpose(0,2,1),
                                 d_eri)
+                t5 = tm.time()
+                self.ostream.print_info("Two electron integral derivatives"
+                                    + ' computed in'
+                                    + ' {:.2f} sec.'.format(t5 - t4))
+                self.ostream.print_blank()
+                self.ostream.flush()
 
         # TODO: enable multiple DMs for DFT to avoid for-loops.
         if self.dft:
@@ -392,7 +413,6 @@ class TddftGradientDriver(GradientDriver):
 
         if isinstance(self.state_deriv_index, int):
             # Python numbering starts at 0
-            print("!!!!! Int!")
             state_deriv_index = self.state_deriv_index - 1
         else:
             state_deriv_index = self.state_deriv_index[0] - 1