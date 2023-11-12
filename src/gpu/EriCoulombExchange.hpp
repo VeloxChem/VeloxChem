529dbefd233710ee2605a220aaa209fcf264b06e
Xin Li <lixin.reco@gmail.com>
2023-11-12 13:00:47 +0100

    save work on SPPS exchange

--- src/gpu/EriCoulombExchange.hpp
+++ src/gpu/EriCoulombExchange.hpp
@@ -289,6 +289,26 @@ __global__ void computeExchangeFockSSPP(double*         mat_K,
                                         const double*   boys_func_table,
                                         const double*   boys_func_ft);
 
+__global__ void computeExchangeFockSPPS(double*         mat_K,
+                                        const uint32_t* pair_inds_i_for_K_sp,
+                                        const uint32_t* pair_inds_k_for_K_sp,
+                                        const uint32_t  pair_inds_count_for_K_sp,
+                                        const double*   s_prim_info,
+                                        const uint32_t* s_prim_aoinds,
+                                        const uint32_t  s_prim_count,
+                                        const double*   p_prim_info,
+                                        const uint32_t* p_prim_aoinds,
+                                        const uint32_t  p_prim_count,
+                                        const double    max_D,
+                                        const double*   mat_D_full_AO,
+                                        const double*   mat_Q_for_K_sp,
+                                        const double*   mat_Q_for_K_ps,
+                                        const uint32_t* density_inds_for_K_sp,
+                                        const uint32_t* density_inds_for_K_ps,
+                                        const uint32_t  naos,
+                                        const double*   boys_func_table,
+                                        const double*   boys_func_ft);
+
 }  // namespace gpu
 
 #endif
