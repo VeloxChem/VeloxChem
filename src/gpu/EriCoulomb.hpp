7df349328242f87e8d7e219ef2ddc92d30e15154
Xin Li <lixin.reco@gmail.com>
2024-03-19 18:55:20 +0200

    test PDPD

--- src/gpu/EriCoulomb.hpp
+++ src/gpu/EriCoulomb.hpp
@@ -2139,6 +2139,24 @@ computeCoulombFockPDPD5(double*         mat_J,
                        const double*   boys_func_ft);
 
 __global__ void
+computeCoulombFockPDPD6(double*         mat_J,
+                       const double*   p_prim_info,
+                       const uint32_t  p_prim_count,
+                       const double*   d_prim_info,
+                       const uint32_t  d_prim_count,
+                       const double*   pd_mat_D,
+                       const double*   pd_mat_Q_local,
+                       const double*   pd_mat_Q,
+                       const uint32_t* pd_first_inds_local,
+                       const uint32_t* pd_second_inds_local,
+                       const uint32_t  pd_prim_pair_count_local,
+                       const uint32_t* pd_first_inds,
+                       const uint32_t* pd_second_inds,
+                       const uint32_t  pd_prim_pair_count,
+                       const double*   boys_func_table,
+                       const double*   boys_func_ft);
+
+__global__ void
 computeCoulombFockPDPP0(double*         mat_J,
                        const double*   p_prim_info,
                        const uint32_t  p_prim_count,
