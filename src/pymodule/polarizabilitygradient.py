c2a5c1f0f520c818201ea5fc53ecd84d286a59f8
johvan <johvan@kth.se>
2024-05-20 16:36:32 +0200

    function for real XC contribution

--- src/pymodule/polarizabilitygradient.py
+++ src/pymodule/polarizabilitygradient.py
@@ -416,11 +416,12 @@ class PolarizabilityGradient():
 
             pol_gradient = self.comm.bcast(pol_gradient, root=mpi_master())
 
-                # Add exchange-correlation contributions to the gradient
-                # for now by looping over each component of the polarizability
+            # Add exchange-correlation contributions to the gradient
+            # for now by looping over each component of the polarizability
             if self._dft:
                 xcfun_label = self.xcfun.get_func_label()
                 if self.is_complex:
+                    # TODO move to separate function
                     for m in range(dof):
                         for n in range(dof):
                             if self.rank == mpi_master():
@@ -489,6 +490,7 @@ class PolarizabilityGradient():
                             if self.rank == mpi_master():
                                 pol_gradient[m, n] += polgrad_xcgrad
                 else: # Real
+                    # TODO move to separate function
                     for m in range(dof):
                         for n in range(dof):
                             if self.rank == mpi_master():
@@ -1403,6 +1405,73 @@ class PolarizabilityGradient():
 
         scf_drv.ostream.unmute()
         cpp_drv.ostream.unmute()
+
+    # TODO work in progress
+    def add_polgrad_xc_contrib_real(self, molecule, ao_basis, gs_dm, rel_dm_ao,
+                                    x_minus_y):
+        """
+        Adds the exchange-correlation contribution to the real
+        polarizability gradient.
+
+        :param molecule:
+            The molecule.
+        :param ao_basis:
+            The AO basis.
+        :param gs_dm:
+            The ground state density matrix.
+        :param rel_dm_ao:
+            The relaxed density matric in AO basis.
+        :param x_minus_y:
+            The X-Y response vector.
+        """
+
+        natm = molecule.number_of_atoms()
+        dof = x_minus_y.shape[0]
+        xc_pol_gradient = np.zeros((dof, dof, natm, 3))
+
+        for m in range(dof):
+            for n in range(dof):
+                if self.rank == mpi_master():
+                    gs_density = AODensityMatrix([gs_dm], denmat.rest)
+
+                    rhow_dm = 1.0 * rel_dm_ao[m, n]
+                    rhow_dm_sym = 0.5 * (rhow_dm + rhow_dm.T)
+                    rhow_den_sym = AODensityMatrix([rhow_dm_sym],
+                                                   denmat.rest)
+
+                    # The sqrt2 takes into account the fact that we need to
+                    # symmetrize with respect to the polarizability
+                    # components.
+                    # (see contraction with two-electron integrals above).
+                    x_minus_y_sym_m = np.sqrt(2) * 0.5 * (x_minus_y[m] +
+                                                          x_minus_y[m].T)
+                    x_minus_y_den_sym_m = AODensityMatrix([x_minus_y_sym_m],
+                                                          denmat.rest)
+                    x_minus_y_sym_n = np.sqrt(2) * 0.5 * (x_minus_y[n] +
+                                                          x_minus_y[n].T)
+                    x_minus_y_den_sym_n = AODensityMatrix([x_minus_y_sym_n],
+                                                          denmat.rest)
+
+                else:
+                    gs_density = AODensityMatrix()
+                    rhow_den_sym = AODensityMatrix()
+                    x_minus_y_den_sym_m = AODensityMatrix()
+                    x_minus_y_den_sym_n = AODensityMatrix()
+
+                gs_density.broadcast(self.rank, self.comm)
+                rhow_den_sym.broadcast(self.rank, self.comm)
+                x_minus_y_den_sym_m.broadcast(self.rank, self.comm)
+                x_minus_y_den_sym_n.broadcast(self.rank, self.comm)
+
+                polgrad_xcgrad = self.grad_polgrad_xc_contrib_real(
+                    molecule, basis, rhow_den_sym,
+                    x_minus_y_den_sym_m, x_minus_y_den_sym_n,
+                    gs_density, xcfun_label)
+
+                if self.rank == mpi_master():
+                    xc_pol_gradient[m, n] += polgrad_xcgrad
+
+        return xc_pol_gradient
 
     def grad_polgrad_xc_contrib_real(self, molecule, ao_basis, rhow_den, x_minus_y_den_m,
                                      x_minus_y_den_n, gs_density, xcfun_label):
