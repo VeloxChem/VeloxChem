b4df76b90bcf290849105e313691713b637265a5
Xin Li <lixin.reco@gmail.com>
2024-09-21 09:46:18 +0200

    added ElectricFieldIntegrals and ElectricFieldValues

---
+++ src/onee_ints/BoysFuncTable.hpp
@@ -0,0 +1,44 @@
+//
+//                              VELOXCHEM
+//         ----------------------------------------------------
+//                     An Electronic Structure Code
+//
+//  Copyright © 2018-2024 by VeloxChem developers. All rights reserved.
+//
+//  SPDX-License-Identifier: LGPL-3.0-or-later
+//
+//  This file is part of VeloxChem.
+//
+//  VeloxChem is free software: you can redistribute it and/or modify it under
+//  the terms of the GNU Lesser General Public License as published by the Free
+//  Software Foundation, either version 3 of the License, or (at your option)
+//  any later version.
+//
+//  VeloxChem is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+//  License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with VeloxChem. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef BoysFuncTable_hpp
+#define BoysFuncTable_hpp
+
+#include <array>
+#include <cstdint>
+#include <vector>
+
+namespace onee {
+
+auto getBoysFuncFactors() -> std::vector<double>;
+
+auto getFullBoysFuncTable() -> std::vector<double>;
+
+auto getBoysFuncTable(const int N) -> std::array<std::array<double, 7>, 121>;
+
+auto computeBoysFunction(double* values, const double fa, const uint32_t N, const double* bf_table, const double* ft) -> void;
+
+}  // namespace onee
+
+#endif /* BoysFuncTable_hpp */
--- src/python/ExportOneElecInts.cpp
+++ src/python/ExportOneElecInts.cpp
@@ -6,6 +6,8 @@
 #include <pybind11/stl.h>
 
 #include "AngularMomentumIntegrals.hpp"
+#include "ElectricFieldIntegrals.hpp"
+#include "ElectricFieldValues.hpp"
 #include "ExportGeneral.hpp"
 #include "ErrorHandler.hpp"
 #include "LinearMomentumIntegrals.hpp"
@@ -44,6 +46,46 @@ export_oneeints(py::module& m)
                 return ret;
             },
             "Computes angular momentum integrals.");
+
+    m.def("compute_electric_field_integrals",
+            [](const CMolecule&           molecule,
+               const CMolecularBasis&     basis,
+               const py::array_t<double>& dipole_coords,
+               const py::array_t<double>& dipole_moments) -> py::array_t<double> {
+                std::string errstyle("compute_electric_field_integrals: Expecting contiguous numpy arrays");
+                auto        c_style_coords  = py::detail::check_flags(dipole_coords.ptr(), py::array::c_style);
+                auto        c_style_moments = py::detail::check_flags(dipole_moments.ptr(), py::array::c_style);
+                errors::assertMsgCritical((c_style_coords && c_style_moments), errstyle);
+                std::string errsize("compute_electric_field_integrals: Inconsistent number of dipole sites");
+                errors::assertMsgCritical(
+                        ((dipole_coords.shape(0) == dipole_moments.shape(0)) &&
+                         (dipole_coords.shape(1) == 3) &&
+                         (dipole_moments.shape(1) == 3)),
+                        errsize);
+                auto ndipoles = static_cast<int>(dipole_coords.shape(0));
+                auto efield = onee::computeElectricFieldIntegrals(molecule, basis, dipole_coords.data(), dipole_moments.data(), ndipoles);
+                return vlx_general::pointer_to_numpy(efield.values(), {efield.getNumberOfRows(), efield.getNumberOfColumns()});
+            },
+            "Computes electric field integrals.");
+
+    m.def("compute_electric_field_values",
+            [](const CMolecule&           molecule,
+               const CMolecularBasis&     basis,
+               const py::array_t<double>& dipole_coords,
+               const py::array_t<double>& D) -> py::array_t<double> {
+                std::string errstyle("compute_electric_field_values: Expecting contiguous numpy arrays");
+                auto        c_style = py::detail::check_flags(dipole_coords.ptr(), py::array::c_style);
+                errors::assertMsgCritical(c_style, errstyle);
+                std::string errsize("compute_electric_field_values: Inconsistent dimension of dipole coordinates");
+                errors::assertMsgCritical(dipole_coords.shape(1) == 3, errsize);
+                std::string errshape("compute_electric_dipole_values: Expecting square matrix D");
+                errors::assertMsgCritical(D.shape(0) == D.shape(1), errshape);
+                auto ndipoles = static_cast<int>(dipole_coords.shape(0));
+                auto naos = static_cast<int>(D.shape(0));
+                auto ef_vals = onee::computeElectricFieldValues(molecule, basis, dipole_coords.data(), ndipoles, D.data(), naos);
+                return vlx_general::pointer_to_numpy(ef_vals.values(), {ef_vals.getNumberOfRows(), ef_vals.getNumberOfColumns()});
+            },
+            "Computes electric field values.");
 }
 
 }  // namespace vlx_oneeints
