f869248276d616ee816c56e045ac18702ec29411
Xin Li <lixin.reco@gmail.com>
2024-09-22 12:30:39 +0200

    updated tda and rpa tests

--- tests/test_rpa.py
+++ tests/test_rpa.py
@@ -10,7 +10,7 @@ from veloxchem import LinearResponseEigenSolver
 @pytest.mark.solvers
 class TestRPA:
 
-    def run_rpa(self, xcfun_label, ref_exc_enes, ref_osc_str, tol):
+    def run_rpa(self, xcfun_label, basis_label, ref_exc_enes, ref_osc_str, tol):
 
         xyz_string = """3
         xyz
@@ -21,7 +21,6 @@ class TestRPA:
         mol = Molecule.read_xyz_string(xyz_string)
         mol.check_multiplicity()
 
-        basis_label = 'def2-svp'
         bas = MolecularBasis.read(mol, basis_label, ostream=None)
 
         scf_drv = ScfRestrictedDriver()
@@ -41,7 +40,7 @@ class TestRPA:
                 np.abs(ref_osc_str -
                        lr_results['oscillator_strengths'])) < 1.0e-4
 
-    def test_hf(self):
+    def test_hf_svp(self):
 
         ref_exc_enes = np.array(
             [0.33973039, 0.40464346, 0.43325535, 0.49805052, 0.55325390])
@@ -49,9 +48,9 @@ class TestRPA:
         ref_osc_str = np.array(
             [0.023665, 0.000000, 0.097765, 0.086454, 0.291919])
 
-        self.run_rpa('hf', ref_exc_enes, ref_osc_str, 1.0e-6)
+        self.run_rpa('hf', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-6)
 
-    def test_slda(self):
+    def test_slda_svp(self):
 
         ref_exc_enes = np.array(
             [0.27459103, 0.34805152, 0.35179468, 0.43068396, 0.51123472])
@@ -59,9 +58,9 @@ class TestRPA:
         ref_osc_str = np.array(
             [0.018005, 0.000000, 0.075418, 0.059909, 0.258165])
 
-        self.run_rpa('slda', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_rpa('slda', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
 
-    def test_b3lyp(self):
+    def test_b3lyp_svp(self):
 
         ref_exc_enes = np.array(
             [0.27940792, 0.35031697, 0.36276301, 0.43718376, 0.51430795])
@@ -69,9 +68,9 @@ class TestRPA:
         ref_osc_str = np.array(
             [0.018243, 0.000000, 0.078326, 0.061329, 0.272933])
 
-        self.run_rpa('b3lyp', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_rpa('b3lyp', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
 
-    def test_camb3lyp(self):
+    def test_camb3lyp_svp(self):
 
         ref_exc_enes = np.array(
             [0.28249530, 0.35533592, 0.36664012, 0.44312170, 0.51548914])
@@ -79,9 +78,20 @@ class TestRPA:
         ref_osc_str = np.array(
             [0.018219, 0.000000, 0.077391, 0.059649, 0.273690])
 
-        self.run_rpa('cam-b3lyp', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_rpa('cam-b3lyp', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
 
-    def test_tpssh(self):
+    def test_camb3lyp_tzvp(self):
+
+        ref_exc_enes = np.array(
+            [0.27984870, 0.35148791, 0.36310452, 0.43770042, 0.50331834])
+
+        ref_osc_str = np.array(
+            [0.033878, 0.000000, 0.099329, 0.053065, 0.229533])
+
+        self.run_rpa('cam-b3lyp', 'def2-tzvp', ref_exc_enes, ref_osc_str,
+                     1.0e-5)
+
+    def test_tpssh_svp(self):
 
         ref_exc_enes = np.array(
             [0.28898749, 0.36043436, 0.37287451, 0.44801126, 0.52385373])
@@ -89,4 +99,4 @@ class TestRPA:
         ref_osc_str = np.array(
             [0.020158, 0.000000, 0.085796, 0.074117, 0.272050])
 
-        self.run_rpa('tpssh', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_rpa('tpssh', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
--- tests/test_tda.py
+++ tests/test_tda.py
@@ -10,7 +10,7 @@ from veloxchem import TdaEigenSolver
 @pytest.mark.solvers
 class TestTDA:
 
-    def run_tda(self, xcfun_label, ref_exc_enes, ref_osc_str, tol):
+    def run_tda(self, xcfun_label, basis_label, ref_exc_enes, ref_osc_str, tol):
 
         xyz_string = """3
         xyz
@@ -21,7 +21,6 @@ class TestTDA:
         mol = Molecule.read_xyz_string(xyz_string)
         mol.check_multiplicity()
 
-        basis_label = 'def2-svp'
         bas = MolecularBasis.read(mol, basis_label, ostream=None)
 
         scf_drv = ScfRestrictedDriver()
@@ -41,25 +40,25 @@ class TestTDA:
                 np.abs(ref_osc_str -
                        lr_results['oscillator_strengths'])) < 1.0e-4
 
-    def test_hf(self):
+    def test_hf_svp(self):
 
         ref_exc_enes = np.array(
             [0.34189725, 0.40717708, 0.43577895, 0.50150019, 0.55485041])
 
         ref_osc_str = np.array([0.0229, 0.0000, 0.1039, 0.0975, 0.3060])
 
-        self.run_tda('hf', ref_exc_enes, ref_osc_str, 1.0e-6)
+        self.run_tda('hf', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-6)
 
-    def test_b3lyp(self):
+    def test_b3lyp_svp(self):
 
         ref_exc_enes = np.array(
             [0.28045483, 0.35053288, 0.36504833, 0.43904962, 0.51570229])
 
         ref_osc_str = np.array([0.0180, 0.0000, 0.0854, 0.0695, 0.3006])
 
-        self.run_tda('b3lyp', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_tda('b3lyp', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
 
-    def test_camb3lyp(self):
+    def test_camb3lyp_svp(self):
 
         ref_exc_enes = np.array(
             [0.28370223, 0.35557650, 0.36883370, 0.44496286, 0.51683587])
@@ -67,4 +66,15 @@ class TestTDA:
         ref_osc_str = np.array(
             [0.017966, 0.000000, 0.084011, 0.068027, 0.300331])
 
-        self.run_tda('cam-b3lyp', ref_exc_enes, ref_osc_str, 1.0e-5)
+        self.run_tda('cam-b3lyp', 'def2-svp', ref_exc_enes, ref_osc_str, 1.0e-5)
+
+    def test_camb3lyp_tzvp(self):
+
+        ref_exc_enes = np.array(
+            [0.28085567, 0.35172399, 0.36474654, 0.43912355, 0.50442105])
+
+        ref_osc_str = np.array(
+            [0.034572, 0.000000, 0.107302, 0.059653, 0.249276])
+
+        self.run_tda('cam-b3lyp', 'def2-tzvp', ref_exc_enes, ref_osc_str,
+                     1.0e-5)
