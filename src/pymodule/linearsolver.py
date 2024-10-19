bb0c7eafcaa6fb775a6040d4441fa8083dfd354c
Xin Li <lixin.reco@gmail.com>
2024-10-19 16:16:20 +0200

    updated debug info in linearsolver

--- src/pymodule/linearsolver.py
+++ src/pymodule/linearsolver.py
@@ -384,12 +384,15 @@ class LinearSolver:
                 'Using sub-communicators for {}.'.format(valstr))
             self.ostream.print_blank()
         else:
+            self._print_mem_debug_info('before screener')
             if self.rank == mpi_master():
                 screening = T4CScreener()
                 screening.partition(basis, molecule, 'eri')
             else:
                 screening = None
+            self._print_mem_debug_info('after  screener')
             screening = self.comm.bcast(screening, root=mpi_master())
+            self._print_mem_debug_info('after  bcast screener')
 
         return {
             'screening': screening,
@@ -584,6 +587,20 @@ class LinearSolver:
         else:
             self._dist_fock_ung.append(fock_ung, axis=1)
 
+    def _print_mem_debug_info(self, label):
+        """
+        Prints memory debug information.
+
+        :param label:
+            The label of memory debug information.
+        """
+
+        if self._debug:
+            profiler = Profiler()
+            self.ostream.print_info(f'==DEBUG==   available memory {label}: ' +
+                                    profiler.get_available_memory())
+            self.ostream.flush()
+
     def compute(self, molecule, basis, scf_tensors, v_grad=None):
         """
         Solves for the linear equations.
@@ -775,23 +792,12 @@ class LinearSolver:
 
             # form Fock matrices
 
-            if self._debug:
-                if profiler is None:
-                    profiler = Profiler()
-                self.ostream.print_info(
-                    '==DEBUG==   available memory before Fock build: ' +
-                    profiler.get_available_memory())
-                self.ostream.flush()
+            self._print_mem_debug_info('before Fock build')
 
             fock = self._comp_lr_fock(dks, molecule, basis, eri_dict, dft_dict,
                                       pe_dict, profiler)
 
-            if self._debug:
-                self.ostream.print_info(
-                    '==DEBUG==   available memory after  Fock build: ' +
-                    profiler.get_available_memory())
-                self.ostream.print_blank()
-                self.ostream.flush()
+            self._print_mem_debug_info('after  Fock build')
 
             if self.rank == mpi_master():
                 raw_fock_ger = []
@@ -976,9 +982,11 @@ class LinearSolver:
             den_mat_for_fock = make_matrix(basis, mat_t.general)
             den_mat_for_fock.set_values(dens[idx])
 
+            self._print_mem_debug_info('before restgen Fock build')
             fock_mat = fock_drv.compute(screening, den_mat_for_fock, fock_type,
                                         exchange_scaling_factor, 0.0,
                                         thresh_int)
+            self._print_mem_debug_info('after  restgen Fock build')
             fock_np = fock_mat.to_numpy()
 
             if fock_type == 'j':
@@ -987,9 +995,11 @@ class LinearSolver:
 
             if need_omega:
                 # for range-separated functional
+                self._print_mem_debug_info('before restgen erf Fock build')
                 fock_mat = fock_drv.compute(screening, den_mat_for_fock,
                                             'kx_rs', erf_k_coef, omega,
                                             thresh_int)
+                self._print_mem_debug_info('after  restgen erf Fock build')
                 fock_np -= fock_mat.to_numpy()
 
             fock_arrays.append(fock_np)
