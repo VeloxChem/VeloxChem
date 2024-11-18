42e327a0c4b749b627db9f4c97e30610ca7add56
Zilvinas Rinkevicius <rinkevic@kth.se>
2024-11-18 06:51:13 +0100

    * Adding test cases for mixed second order geometrical derivatives of Fock matrices.

--- src/pymodule/__init__.py
+++ src/pymodule/__init__.py
@@ -26,6 +26,7 @@ from .veloxchemlib import ElectricDipoleMomentumGeom100Driver
 from .veloxchemlib import T4CScreener
 from .veloxchemlib import FockGeom1000Driver
 from .veloxchemlib import FockGeom2000Driver
+from .veloxchemlib import FockGeom1100Driver
 
 # C++ functions
 from .veloxchemlib import upper_case
