24ab36bf62ae772e9531e2df69b8b1828c7b3e41
Zilvinas Rinkevicius <rinkevic@kth.se>
2024-11-18 06:50:30 +0100

    * Adding test cases for mixed second order geometrical derivatives of Fock matrices.

---
+++ src/t4c_electron_repulsion_geom_XY00/ElectronRepulsionGeom1100Func.hpp
@@ -0,0 +1,13 @@
+//
+//  ElectronRepulsionGeom1100Func.hpp
+//  VeloxChemLIb
+//
+//  Created by Zilvinas Rinkevicius on 2024-11-17.
+//
+
+#ifndef ElectronRepulsionGeom1100Func_hpp
+#define ElectronRepulsionGeom1100Func_hpp
+
+#include <stdio.h>
+
+#endif /* ElectronRepulsionGeom1100Func_hpp */
---
+++ src/t4c_func/FockGeomXY00Driver.hpp
@@ -0,0 +1,13 @@
+//
+//  FockGeomXY00Driver.hpp
+//  VeloxChemLIb
+//
+//  Created by Zilvinas Rinkevicius on 2024-11-17.
+//
+
+#ifndef FockGeomXY00Driver_hpp
+#define FockGeomXY00Driver_hpp
+
+#include <stdio.h>
+
+#endif /* FockGeomXY00Driver_hpp */
