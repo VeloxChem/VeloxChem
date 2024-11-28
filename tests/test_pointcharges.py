512e0b04416d7534c0f33ab0a85bc3fb804c6d99
Xin Li <lixin.reco@gmail.com>
2024-11-28 11:27:49 +0100

    updated tests/test_pointcharges.py

--- tests/test_pointcharges.py
+++ tests/test_pointcharges.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import numpy as np
+import pytest
 
 from veloxchem.veloxchemlib import mpi_master
 from veloxchem.molecule import Molecule
@@ -9,6 +10,7 @@ from veloxchem.scfunrestdriver import ScfUnrestrictedDriver
 from veloxchem.scfgradientdriver import ScfGradientDriver
 
 
+@pytest.mark.solvers
 class TestPointCharges:
 
     @staticmethod
@@ -26,7 +28,8 @@ class TestPointCharges:
         bas = MolecularBasis.read(mol, basis_label, ostream=None)
         return mol, bas
 
-    def test_scf_with_point_charges(self):
+    def run_scf_with_point_charges(self, xcfun_label, ref_energy, ene_tol,
+                                   ref_grad, grad_tol):
 
         mol, bas = self.get_molecule_and_basis()
 
@@ -36,6 +39,7 @@ class TestPointCharges:
 
         for scf_drv in [ScfRestrictedDriver(), ScfUnrestrictedDriver()]:
 
+            scf_drv.xcfun = xcfun_label
             scf_drv._point_charges = potfile
             scf_drv._qm_vdw_params = vdwfile
             scf_drv.ostream.mute()
@@ -43,16 +47,34 @@ class TestPointCharges:
             scf_results = scf_drv.compute(mol, bas)
 
             if scf_drv.rank == mpi_master():
-                assert abs(scf_results['scf_energy'] - (-75.9732334209)) < 1e-9
+                assert abs(scf_results['scf_energy'] - ref_energy) < ene_tol
 
             grad_drv = ScfGradientDriver(scf_drv)
             grad_drv.compute(mol, bas, scf_results)
             grad = grad_drv.get_gradient()
 
-            ref_grad = np.array([
-                [-0.012426949439, -0.009449823622, -0.013955922853],
-                [-0.003881905016, -0.002485992070, 0.002890530027],
-                [0.010380233206, 0.003716170298, 0.009359249589],
-            ])
+            assert np.max(np.abs(grad - ref_grad)) < grad_tol
 
-            assert np.max(np.abs(grad - ref_grad)) < 1e-6
+    def test_hf_with_point_charges(self):
+
+        ref_energy = -75.9732334209
+        ref_grad = np.array([
+            [-0.012426949439, -0.009449823622, -0.013955922853],
+            [-0.003881905016, -0.002485992070, 0.002890530027],
+            [0.010380233206, 0.003716170298, 0.009359249589],
+        ])
+
+        self.run_scf_with_point_charges('hf', ref_energy, 1.0e-9, ref_grad,
+                                        1.0e-6)
+
+    def test_b3lyp_with_point_charges(self):
+
+        ref_energy = -76.3690497939
+        ref_grad = np.array([
+            [-0.011154306762, -0.008028413518, 0.014921369370],
+            [0.010672309330, -0.000331661909, -0.011756057376],
+            [-0.006021550334, -0.000313431386, -0.004840032815],
+        ])
+
+        self.run_scf_with_point_charges('b3lyp', ref_energy, 1.0e-9, ref_grad,
+                                        1.0e-4)
