39a18eed87b8ac2e1978f78c76adee4c7e1a9087
johvan <johvan@kth.se>
2024-12-17 12:26:47 +0100

    split up real/imag, fixed for compute omega

--- tests/test_cphfpolgrad.py
+++ tests/test_cphfpolgrad.py
@@ -50,6 +50,7 @@ class TestCphfPolgrad:
         cphfpolgrad_solver.update_settings(cphfpolgrad_settings, method_settings)
         cphfpolgrad_solver.ostream.mute()
         cphfpolgrad_solver.compute(molecule, basis, scf_tensors, lr_results)
+        cphfpolgrad_solver.compute_omega(molecule, basis, scf_tensors, lr_results)
 
         if scf_drv.rank == mpi_master():
             cphfpolgrad_results = cphfpolgrad_solver.cphf_results
@@ -100,6 +101,7 @@ class TestCphfPolgrad:
         cphfpolgrad_solver.update_settings(cphfpolgrad_settings, method_settings)
         cphfpolgrad_solver.ostream.mute()
         cphfpolgrad_solver.compute(molecule, basis, scf_tensors, lr_results)
+        cphfpolgrad_solver.compute_omega(molecule, basis, scf_tensors, lr_results)
 
         dist_cphf_coefficients = cphfpolgrad_solver.cphf_results['dist_cphf_ov']
         dof = len(dist_cphf_coefficients)
@@ -163,6 +165,7 @@ class TestCphfPolgrad:
         cphfpolgrad_solver.update_settings(cphfpolgrad_settings, method_settings)
         cphfpolgrad_solver.ostream.mute()
         cphfpolgrad_solver.compute(molecule, basis, scf_tensors, lr_results)
+        cphfpolgrad_solver.compute_omega(molecule, basis, scf_tensors, lr_results)
 
         if scf_drv.rank == mpi_master():
             cphfpolgrad_results = cphfpolgrad_solver.cphf_results
@@ -214,6 +217,7 @@ class TestCphfPolgrad:
         cphfpolgrad_solver.update_settings(cphfpolgrad_settings, method_settings)
         cphfpolgrad_solver.ostream.mute()
         cphfpolgrad_solver.compute(molecule, basis, scf_tensors, lr_results)
+        cphfpolgrad_solver.compute_omega(molecule, basis, scf_tensors, lr_results)
 
         dist_cphf_coefficients = cphfpolgrad_solver.cphf_results['dist_cphf_ov']
         dof = len(dist_cphf_coefficients)
@@ -247,7 +251,7 @@ class TestCphfPolgrad:
 
 #    @pytest.mark.skipif('pyscf' not in sys.modules,
 #                        reason='pyscf for integral derivatives not available')
-    def test_cphfpolgrad_coefficients(self):
+    def test_cphfpolgrad_coefficients_real(self):
         h2o_xyz = """3
 
         O     0.000000    0.000000    0.000000
@@ -264,6 +268,18 @@ class TestCphfPolgrad:
                                   "cphfpolgrad_omega_coefficients_real")
         self.run_cphfpolgrad_ss_real(molecule, basis, None, "cphfpolgrad_coefficients_red_real",
                                   "cphfpolgrad_omega_coefficients_real")
+
+    def test_cphfpolgrad_coefficients_complex(self):
+        h2o_xyz = """3
+
+        O     0.000000    0.000000    0.000000
+        H     0.000000    0.504284    0.758602
+        H     0.000000   -0.504284    0.758602 
+        """
+        basis_set_label = "sto-3g"
+
+        molecule = Molecule.from_xyz_string(h2o_xyz)
+        basis = MolecularBasis.read(molecule, basis_set_label)
 
         self.run_cphfpolgrad_cg_complex(molecule, basis, None, "cphfpolgrad_coefficients_red_complex",
                                      "cphfpolgrad_omega_coefficients_complex")
@@ -289,6 +305,18 @@ class TestCphfPolgrad:
                                   "cpkspolgrad_omega_coefficients_b3lyp_real")
         self.run_cphfpolgrad_ss_real(molecule, basis, "b3lyp", "cpkspolgrad_coefficients_red_b3lyp_real",
                                   "cpkspolgrad_omega_coefficients_b3lyp_real")
+
+    def test_cpkspolgrad_coefficients_complex(self):
+        h2o_xyz = """3
+
+        O     0.000000    0.000000    0.000000
+        H     0.000000    0.504284    0.758602
+        H     0.000000   -0.504284    0.758602 
+        """
+        basis_set_label = "sto-3g"
+
+        molecule = Molecule.from_xyz_string(h2o_xyz)
+        basis = MolecularBasis.read(molecule, basis_set_label)
 
         self.run_cphfpolgrad_cg_complex(molecule, basis, "b3lyp", "cpkspolgrad_coefficients_red_b3lyp_complex",
                                      "cpkspolgrad_omega_coefficients_b3lyp_complex")
