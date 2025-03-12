53916b996447689f51678c89ce373a3f66725a9c
Bastiaan van Hoorn <bvh@kth.se>
2025-03-12 11:07:08 +0000

    Created an empty class

---
+++ src/pymodule/tsguesser.py
@@ -0,0 +1,61 @@
+#
+#                              VELOXCHEM
+#         ----------------------------------------------------
+#                     An Electronic Structure Code
+#
+#  Copyright © 2018-2024 by VeloxChem developers. All rights reserved.
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+#  This file is part of VeloxChem.
+#
+#  VeloxChem is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Lesser General Public License as published by the Free
+#  Software Foundation, either version 3 of the License, or (at your option)
+#  any later version.
+#
+#  VeloxChem is distributed in the hope that it will be useful, but WITHOUT
+#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+#  License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with VeloxChem. If not, see <https://www.gnu.org/licenses/>.
+from mpi4py import MPI
+import sys
+
+from .veloxchemlib import mpi_master
+from .outputstream import OutputStream
+from .errorhandler import assert_msg_critical
+from .molecule import Molecule
+
+try:
+    import openmm as mm
+    import openmm.app as mmapp
+    import openmm.unit as mmunit
+except ImportError:
+    pass
+
+
+class TransitionStateGuesser():
+
+    def __init__(self, comm=None, ostream=None):
+        '''
+        Initialize the Transition State Guesser class.
+        '''
+        if comm is None:
+            comm = MPI.COMM_WORLD
+
+        if ostream is None:
+            if comm.Get_rank() == mpi_master():
+                ostream = OutputStream(sys.stdout)
+            else:
+                ostream = OutputStream(None)
+
+        # output stream
+        self.ostream = ostream
+
+        # mpi information
+        self.comm = comm
+        self.rank = self.comm.Get_rank()
+        self.nodes = self.comm.Get_size()
