a14ad890dd0f4e4f5aa76a9f4f85ec3f33a518d7
Paul Bauer <paul.bauer@amd.com>
2025-03-28 03:49:58 -0500

    Ensure methods are properly inlined
    Gives ~5% perf for free

--- src/gpu/BoysFuncGPU.hpp
+++ src/gpu/BoysFuncGPU.hpp
@@ -54,7 +54,7 @@ private:
     ValueType* buffer;
 
 public:
-    __device__ AmdFastBuffer(ValueType* buffer) : buffer(buffer) {}
+    __device__ __attribute__((always_inline)) AmdFastBuffer(ValueType* buffer) : buffer(buffer) {}
     template<typename IndexType, std::enable_if_t<std::is_integral<IndexType>::value && std::is_const_v<std::remove_pointer_t<ValueType>>, bool> = true>
     inline __device__ __attribute__((always_inline)) const ValueType& operator[](IndexType idx) const
     {
@@ -67,7 +67,7 @@ public:
     }
 };
 
-__device__ void
+__device__ __attribute__((always_inline)) void
 computeBoysFunction(double* values_in, const double fa, const uint32_t N, const double* bf_table, const double* ft_in)
 {
     // Note: 847 = 121 * 7
