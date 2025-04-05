0899f18fbe25f45233f6044e9a902bc15eac8061
Erik Vitols <erik.vitols@proton.me>
2025-04-05 12:21:43 +0200

    including local alternative rixs-driver for memory-intensive tasks, will be combined later

--- src/pymodule/__init__.py
+++ src/pymodule/__init__.py
@@ -149,8 +149,9 @@ from .rspabsorption import Absorption
 from .rspc6 import C6
 from .rspshg import SHG
 from .rsptpa import TPA
+from .rixsdriverold import oldRixsDriver
 from .rixsdriver import RixsDriver
-from .rixsdrivernew import newRixsDriver
+from .altrixsdriver import altRixsDriver
 #from .rspcustomproperty import CustomProperty
 from .mpitask import MpiTask
 from .subcommunicators import SubCommunicators
