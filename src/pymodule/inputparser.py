4692c98f2563bf994010aaee4ad1a50494b825f4
Xin Li <lixin.reco@gmail.com>
2025-04-07 13:54:48 +0200

    minor update in random string in inputparser

--- src/pymodule/inputparser.py
+++ src/pymodule/inputparser.py
@@ -524,8 +524,7 @@ def get_random_string_serial():
 
     datetime_string = datetime.now().isoformat(sep='T', timespec='seconds')
     datetime_string = datetime_string.replace('-', '')
-    datetime_string = datetime_string.replace('T', '_')
-    datetime_string = datetime_string.replace(':', '')
+    datetime_string = datetime_string.split('T')[0]
 
     random_string = '{:>08s}'.format(hex(getrandbits(32))[2:])
 
