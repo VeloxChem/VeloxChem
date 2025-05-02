2d1919f0ebe2dd2ad3c8cf3a2f2cf09fd64bf7f0
Magnus Johansen <qhw298@fend04.cluster>
2025-05-02 11:26:41 +0200

    Added tests for CPP and excitations with C-PCM

---
+++ tests/test_cpcm_cpp.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pytest
+
+from veloxchem.molecule import Molecule
+from veloxchem.molecularbasis import MolecularBasis
+from veloxchem.scfrestdriver import ScfRestrictedDriver
+from veloxchem.cppsolver import ComplexResponse
+
+
+@pytest.mark.solvers
+class TestCPPCpcm:
+
+    def run_cpp_cpcm(self,
+                     xcfun_label,
+                     cpp_flag,
+                     ref_x_data,
+                     ref_y_data,
+                     tol,
+                     noneq_solv=False):
+
+        xyz_string = """6
+        xyz
+        O -3.42904  1.55532  0.01546
+        C -1.99249  1.74379  0.02665
+        H -1.74709  2.74160  0.44749
+        H -1.59636  1.67836 -1.00861
+        H -1.51398  0.95881  0.64937
+        H -3.84726  2.33620 -0.34927
+        """
+        mol = Molecule.read_xyz_string(xyz_string)
+
+        basis_label = 'def2-svp'
+        bas = MolecularBasis.read(mol, basis_label, ostream=None)
+
+        scf_drv = ScfRestrictedDriver()
+        scf_drv.ostream.mute()
+        scf_drv.xcfun = xcfun_label
+        scf_results = scf_drv.compute(mol, bas)
+
+        lr_drv = ComplexResponse()
+        lr_drv.ostream.mute()
+        lr_drv.set_cpp_flag(cpp_flag)
+        lr_drv.frequencies = list(ref_x_data)
+        lr_drv.solvation_model = 'cpcm'
+        lr_drv.cpcm_grid_per_sphere = (110, 110)
+        lr_drv.non_equilibrium_solv = noneq_solv
+        lr_results = lr_drv.compute(mol, bas, scf_results)
+
+        lr_spec = lr_drv.get_spectrum(lr_results, 'au')
+        assert np.max(
+            np.abs(np.array(lr_spec['y_data']) -
+                    np.array(ref_y_data))) < tol
+
+    def test_b3lyp_absorption_cpcm(self):
+
+        # vlxtag: RKS, Absorption, CPP, CPCM
+
+        xcfun_label = 'b3lyp'
+        cpp_flag = 'absorption'
+        ref_x_data = [0.39, 0.40, 0.41]
+        ref_y_data = [0.32158440, 0.66111297, 2.15027316]
+
+        self.run_cpp_cpcm(xcfun_label, cpp_flag, ref_x_data, ref_y_data, 1.0e-4)
+
+    def test_b3lyp_absorption_cpcm_noneq(self):
+
+        # vlxtag: RKS, Absorption, CPP, CPCM
+
+        xcfun_label = 'b3lyp'
+        cpp_flag = 'absorption'
+        ref_x_data = [0.39, 0.40, 0.41]
+        ref_y_data = [0.23785824, 0.32046951, 1.27698344]
+
+        self.run_cpp_cpcm(xcfun_label, cpp_flag, ref_x_data, ref_y_data, 1.0e-4,
+                          noneq_solv=True)
---
+++ tests/test_cpcm_excitation.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pytest
+
+from veloxchem.molecule import Molecule
+from veloxchem.molecularbasis import MolecularBasis
+from veloxchem.scfrestdriver import ScfRestrictedDriver
+from veloxchem.lreigensolver import LinearResponseEigenSolver
+from veloxchem.tdaeigensolver import TdaEigenSolver
+
+
+@pytest.mark.solvers
+class TestExcitationCpcm:
+
+    def run_cpcm_exc(self, molecule, xcfun_label, basis_label, ref_exc, ref_osc,
+                 cpcm_custom_vdw_radii, tol, tda=False, noneq_solv = False):
+
+        basis = MolecularBasis.read(molecule, basis_label)
+
+        scf_drv = ScfRestrictedDriver()
+        scf_drv.xcfun = xcfun_label
+
+        scf_drv.solvation_model = 'cpcm'
+        scf_drv.cpcm_grid_per_sphere = (110, 110)
+        scf_drv.cpcm_custom_vdw_radii = cpcm_custom_vdw_radii
+
+        scf_drv.ostream.mute()
+        scf_results = scf_drv.compute(molecule, basis)
+
+        if tda:
+            rsp_drv = TdaEigenSolver()
+        else:
+            rsp_drv = LinearResponseEigenSolver()
+
+        rsp_drv.ostream.mute()
+        rsp_drv.nstates = 5
+        rsp_drv.solvation_model = 'cpcm'
+        rsp_drv.cpcm_grid_per_sphere = (110, 110)
+        rsp_drv.cpcm_custom_vdw_radii = cpcm_custom_vdw_radii
+        rsp_drv.non_equilibrium_solv = noneq_solv
+
+        rsp_results = rsp_drv.compute(molecule, basis, scf_results)
+        exc = rsp_results['eigenvalues']
+        osc = rsp_results['oscillator_strengths']
+
+        assert np.max(np.abs(exc - ref_exc)) < tol
+        assert np.max(np.abs(osc - ref_osc)) < tol
+
+
+    def test_nh3_def2svp(self):
+
+        molstr = """
+        N         -1.96309        1.59755       -0.01963
+        H         -1.95876        2.61528        0.03109
+        H         -2.48929        1.27814        0.79244
+        H         -2.52930        1.35928       -0.83265
+        """
+        mol = Molecule.read_molecule_string(molstr, units='bohr')
+
+        # LR-HF
+
+        ref_exc = np.array([0.40521822, 0.4860046,  0.48603203, 0.77782504, 0.77783245])
+        ref_osc = np.array([0.06158212, 0.01578646, 0.01578704, 0.12326755, 0.12328609])
+        self.run_cpcm_exc(mol, 'hf', 'def2-svp', ref_exc, ref_osc,  None, 1.0e-5)
+
+        # TDA-HF
+
+        ref_exc = np.array([0.40578896, 0.48668494, 0.48671237, 0.77825217, 0.7782596])
+        ref_osc = np.array([0.06022241, 0.01533443, 0.01533621, 0.11997976, 0.11999964])
+        self.run_cpcm_exc(mol, 'hf', 'def2-svp', ref_exc, ref_osc,  None,
+                           1.0e-5,tda=True)
+
+        # LR-HF + Non-eq. solvation
+
+        ref_exc = np.array([0.40759806, 0.48702943, 0.48705686, 0.78026947, 0.78027528])
+        ref_osc = np.array([0.05766621, 0.01458069, 0.0145834,  0.11290953, 0.11292115])
+        self.run_cpcm_exc(mol, 'hf', 'def2-svp', ref_exc, ref_osc,  None,
+                           1.0e-5, noneq_solv=True)
+
+        # LR-DFT
+
+        ref_exc = np.array([0.34592948, 0.42604827, 0.42607682, 0.68311411, 0.68312656])
+        ref_osc = np.array([0.04423422, 0.01472548, 0.01472579, 0.09518219, 0.095212  ])
+        self.run_cpcm_exc(mol, 'b3lyp', 'def2-svp', ref_exc, ref_osc,  None, 1.0e-5)
+
+        # LR-HF + custom vdW radii
+
+        ref_exc = np.array([0.40041427, 0.4813658,  0.48136975, 0.7765264,  0.7765328])
+        ref_osc = np.array([0.05829868, 0.0139734,  0.01397378, 0.11872162, 0.11872378])
+        self.run_cpcm_exc(mol, 'hf', 'def2-svp', ref_exc, ref_osc,  ['N', 1.8], 1.0e-5)
