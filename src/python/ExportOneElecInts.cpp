cebba068afba9a9d7b0e59d9a666689258cd2f19
Xin Li <lixin.reco@gmail.com>
2025-06-04 14:59:07 +0200

    added compute_tco_s_fock

--- src/python/ExportOneElecInts.cpp
+++ src/python/ExportOneElecInts.cpp
@@ -55,6 +55,7 @@
 #include "OldOneElecIntsDrivers.hpp"
 #include "OverlapHessian101.hpp"
 #include "KineticEnergyHessian101.hpp"
+#include "TCOSFock1GOST.hpp"
 
 namespace py = pybind11;
 using namespace py::literals;
@@ -513,6 +514,33 @@ export_oneeints(py::module& m)
              "molecule"_a,
              "basis"_a,
              "origin"_a = std::vector<double>({0.0, 0.0, 0.0}));
+
+    m.def("compute_tco_s_fock",
+            [](const CMolecule&           molecule,
+               const CMolecularBasis&     basis,
+               const py::array_t<double>& point_coords,
+               const py::array_t<double>& point_exp,
+               const py::array_t<double>& point_amp) -> py::array_t<double> {
+                std::string errstyle("compute_tco_s_fock: Expecting contiguous numpy arrays");
+                auto        c_style_1 = py::detail::check_flags(point_coords.ptr(), py::array::c_style);
+                auto        c_style_2 = py::detail::check_flags(point_exp.ptr(), py::array::c_style);
+                auto        c_style_3 = py::detail::check_flags(point_amp.ptr(), py::array::c_style);
+                errors::assertMsgCritical((c_style_1 && c_style_2 && c_style_3), errstyle);
+                std::string errsize("compute_tco_s_fock: Inconsistent sizes");
+                errors::assertMsgCritical(point_coords.shape(0) == point_exp.shape(0), errsize);
+                errors::assertMsgCritical(point_coords.shape(0) == point_amp.shape(0), errsize);
+                errors::assertMsgCritical(point_coords.shape(1) == 3, errsize);
+                auto npoints = static_cast<int>(point_coords.shape(0));
+                auto naos = static_cast<int>(basis.dimensions_of_basis());
+                auto tco_s = onee::computeFock1GOSTcontrib(molecule, basis, point_coords.data(), npoints, point_exp.data(), point_amp.data(), naos);
+                return vlx_general::pointer_to_numpy(tco_s.values(), {tco_s.getNumberOfRows(), tco_s.getNumberOfColumns()});
+            },
+            "Computes three-center s-type overlap integrals.",
+             "molecule"_a,
+             "basis"_a,
+             "point_coords"_a,
+             "point_exp"_a,
+             "point_amp"_a);
 }
 
 }  // namespace vlx_oneeints
