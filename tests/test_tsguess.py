ea470a78782ac509d16cc25a502ea1df1db60aa1
Bastiaan van Hoorn <bvh@kth.se>
2025-06-04 12:25:56 +0000

    Some small fixes in the testing

--- tests/test_tsguess.py
+++ tests/test_tsguess.py
@@ -33,38 +33,11 @@ class TestTransitionStateGuesser:
         folder = Path(__file__).parent / 'data'
         reference_results = evb._load_dict_from_h5(
             folder / "ts_guesser_reference_results.h5")
-        self._compare_dict(results, reference_results)
 
-    def _compare_dict(self, dict1, dict2, float_tol=1):
-
-        assert sorted(list(dict1.keys())) == sorted(list(dict2.keys()))
-
-        for key in dict1:
-
-            if key == 'comment' or key == 'mm_energies' or key == 'xyz_geometries' or key == 'final_geometry':
-                continue
-
-            val1 = dict1[key]
-            val2 = dict2[key]
-
-            type1 = type(val1)
-            type2 = type(val2)
-
-            # try to convert val2
-            if type1 != type2:
-                try:
-                    val2 = type1(val2)
-                except (ValueError, TypeError):
-                    print(
-                        f"Type mismatch: {type1} != {type(val2)} for key {key}")
-                    assert False
-
-            # compare val1 with val2
-            if type1 is dict:
-                self._compare_dict(val1, val2)
-            elif type1 is float or type1 is np.float64:
-                assert abs(val1 - val2) < float_tol
-            elif type1 is list or type1 is np.ndarray:
-                assert np.allclose(val1, val2, atol=float_tol)
-            else:
-                assert val1 == val2
+        assert results['broken_bonds'] == {
+            tuple(reference_results['broken_bonds'][0])
+        }
+        assert results['formed_bonds'] == {
+            tuple(reference_results['formed_bonds'][0])
+        }
+        assert results['final_lambda'] == reference_results['final_lambda']
