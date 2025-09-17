873316e5c282d14426236b876905e5cad9a4c58d
caseyli01 <93268539+caseyli01@users.noreply.github.com>
2025-09-17 13:53:27 +0200

    update test_atombdedriver.py
    use xyz_string instead of smiles. check all ranks. unlink the generated h5 files

--- tests/test_atombdedriver.py
+++ tests/test_atombdedriver.py
@@ -1,16 +1,10 @@
 import pytest
-import sys
-
+from pathlib import Path
 from veloxchem.veloxchemlib import mpi_master
 from veloxchem.veloxchemlib import Molecule
 from veloxchem.molecularbasis import MolecularBasis
 from veloxchem.scfrestdriver import ScfRestrictedDriver
 from veloxchem.atombdedriver import AtomBdeDriver
-
-try:
-    import rdkit
-except ImportError:
-    pass
 
 
 class TestAtomBdeDriver:
@@ -18,13 +12,36 @@ class TestAtomBdeDriver:
     def run_atom_bde(self, molecules):
         bde = AtomBdeDriver()
         bde.mute_output = True
-        bde.save_files = False
         bde.compute(molecules)
+        #check all ranks
         assert abs(bde.mols_bdes_list[0]["unique_BDEs_hartree"][0] -
                        0.17657796026960781) < 1.0e-4
 
+    def cleanup_h5files(self):
+        files=['bde_mol_1_1_final_scf.h5',
+        'bde_mol_1_1_final.h5',
+        'bde_mol_1_1_opt_scf.h5',
+        'bde_mol_1_1_opt.h5',
+        'bde_mol_1_final_scf.h5',
+        'bde_mol_1_final.h5',
+        'bde_mol_1_opt_scf.h5',
+        'bde_mol_1_opt.h5',
+        'bde_mol_1_target_H_final_scf.h5',
+        'bde_mol_1_target_H_final.h5']
+        for f in files:
+            p=Path(f)
+            if p.is_file():
+                p.unlink()
 
-    @pytest.mark.skipif("rdkit" not in sys.modules,
-                        reason="rdkit not available")
     def test_atom_bde_driver(self):
-        self.run_atom_bde(Molecule.read_smiles('C'))
+        #use a methane molecule as test case
+        xyz_str = """
+            5
+            C              1.529985000000        -0.102981000000        -0.995481000000
+            H              2.583412000000        -0.313470000000        -0.718431000000
+            H              1.140736000000         0.724598000000        -0.367493000000
+            H              0.915727000000        -1.012123000000        -0.831416000000
+            H              1.480064000000         0.189072000000        -2.064585000000
+            """
+        self.run_atom_bde(Molecule.read_xyz_string(xyz_str))
+        self.cleanup_h5files()
