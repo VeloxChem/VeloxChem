2e41fa066cca33a7b379bb935f0ab571ca46c080
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2026-02-10 13:34:43 +0100

    changed EnvironmentDriver for EnsembleDriver

--- src/pymodule/__init__.py
+++ src/pymodule/__init__.py
@@ -214,7 +214,7 @@ from .reactionmatcher import ReactionMatcher
 from .smddriver import SmdDriver
 from .atombdedriver import AtomBdeDriver
 from .ensembleparser import EnsembleParser
-from .environmentdriver import EnvironmentDriver
+from .ensembledriver import EnsembleDriver
 from .spectraaverager import SpectraAverager
 # for backward compatibility only
 from .peforcefieldgenerator import PEForceFieldGenerator as LoPropDriver
