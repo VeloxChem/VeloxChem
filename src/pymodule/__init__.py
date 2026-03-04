bf9ef1853db641e4e107fba35c206454d15f7dd4
David Carrasco de Busturia <davidcarrascobustur@gmail.com>
2026-03-04 09:11:31 +0100

    replace name SpectraAverager by SpectrumAverager

--- src/pymodule/__init__.py
+++ src/pymodule/__init__.py
@@ -215,7 +215,7 @@ from .smddriver import SmdDriver
 from .atombdedriver import AtomBdeDriver
 from .ensembleparser import EnsembleParser
 from .ensembledriver import EnsembleDriver
-from .spectraaverager import SpectraAverager
+from .spectrumaverager import SpectrumAverager
 # for backward compatibility only
 from .peforcefieldgenerator import PEForceFieldGenerator as LoPropDriver
 
