8df5eef1777bc298e0ffc7ae5064487d62917ea6
Xin Li <lixin.reco@gmail.com>
2026-04-03 09:34:47 +0200

    updated RPA tests

--- tests/test_rpa.py
+++ tests/test_rpa.py
@@ -1,4 +1,5 @@
 import numpy as np
+import h5py
 from mpi4py import MPI
 import pytest
 
@@ -235,3 +236,119 @@ class TestRPA:
                 assert np.max(
                     np.abs(lr_results_first[key] -
                            lr_results_second[key])) < tol
+
+    def test_restart_allows_missing_nstates(self, tmp_path):
+
+        xyz_string = """3
+        xyz
+        O   -0.1858140  -1.1749469   0.7662596
+        H   -0.1285513  -0.8984365   1.6808606
+        H   -0.0582782  -0.3702550   0.2638279
+        """
+        mol = Molecule.read_xyz_string(xyz_string)
+        bas = MolecularBasis.read(mol, 'sto-3g', ostream=None)
+
+        scf_drv = ScfRestrictedDriver()
+        scf_drv.ostream.mute()
+
+        filename = str(tmp_path / 'restricted_missing_nstates')
+        filename = scf_drv.comm.bcast(filename, root=mpi_master())
+
+        scf_drv.filename = filename
+        scf_drv.compute(mol, bas)
+        scf_drv.restart = True
+        scf_results = scf_drv.compute(mol, bas)
+        scf_results['filename'] = filename
+
+        reference_drv = LinearResponseEigenSolver()
+        reference_drv.ostream.mute()
+        reference_drv.filename = filename
+        reference_drv.conv_thresh = 1.0e-5
+        reference_drv.max_iter = 60
+        reference_drv.nstates = 2
+        reference_drv.initial_guess_multiplier = 2
+        reference_drv.max_subspace_dim = 8
+        reference_drv.ri_auxiliary_basis = 'def2-universal-jfit'
+        reference_drv.compute(mol, bas, scf_results)
+
+        if reference_drv.rank == mpi_master():
+            with h5py.File(reference_drv.checkpoint_file, 'a') as h5f:
+                del h5f['nstates']
+
+        reference_drv.comm.barrier()
+
+        restarted_drv = LinearResponseEigenSolver()
+        restarted_drv.ostream.mute()
+        restarted_drv.filename = filename
+        restarted_drv.checkpoint_file = reference_drv.checkpoint_file
+        restarted_drv.conv_thresh = 1.0e-5
+        restarted_drv.max_iter = 60
+        restarted_drv.nstates = 2
+        restarted_drv.initial_guess_multiplier = 2
+        restarted_drv.max_subspace_dim = 8
+        restarted_drv.ri_auxiliary_basis = 'def2-universal-jfit'
+        restarted_drv.restart = True
+
+        restarted_results = restarted_drv.compute(mol, bas, scf_results)
+
+        assert restarted_drv.restart is True
+
+        if restarted_drv.rank == mpi_master():
+            assert restarted_results['number_of_states'] == restarted_drv.nstates
+
+    def test_get_e2_h2(self):
+
+        xyz_string = """2
+        xyz
+        H   0.000000   0.000000   0.000000
+        H   0.000000   0.000000   0.740000
+        """
+        mol = Molecule.read_xyz_string(xyz_string)
+        bas = MolecularBasis.read(mol, 'sto-3g', ostream=None)
+
+        scf_drv = ScfRestrictedDriver()
+        scf_drv.ostream.mute()
+        scf_results = scf_drv.compute(mol, bas)
+
+        lr_drv = LinearResponseEigenSolver()
+        lr_drv.ostream.mute()
+        e2_matrix = lr_drv.get_e2(mol, bas, scf_results)
+
+        if lr_drv.rank == mpi_master():
+            assert e2_matrix.shape == (2, 2)
+            assert np.max(np.abs(e2_matrix - e2_matrix.T)) < 1.0e-10
+            assert np.all(np.diag(e2_matrix) > 0.0)
+
+    def test_guess_and_preconditioner_helpers(self):
+
+        lr_drv = LinearResponseEigenSolver()
+        lr_drv.ostream.mute()
+        lr_drv.nstates = 2
+        lr_drv.initial_guess_multiplier = 2
+        lr_drv.guess_scaling_threshold = 5
+
+        full_guesses = lr_drv._initial_excitations(2, np.array([-0.9, -0.4, 0.2]),
+                                                   1, 3)
+        assert len(full_guesses) == 2
+        assert full_guesses[0].data.shape == (2, 2)
+
+        lr_drv.core_excitation = True
+        lr_drv.num_core_orbitals = 1
+        core_guesses = lr_drv._initial_excitations(1,
+                                                   np.array([-0.9, -0.4, 0.2]),
+                                                   2, 3)
+        assert len(core_guesses) == 1
+
+        lr_drv.core_excitation = False
+        lr_drv.restricted_subspace = True
+        lr_drv.num_core_orbitals = 1
+        lr_drv.num_valence_orbitals = 1
+        lr_drv.num_virtual_orbitals = 1
+        restricted_guesses = lr_drv._initial_excitations(
+            1, np.array([-0.9, -0.4, 0.2, 0.6]), 2, 4)
+        assert len(restricted_guesses) == 2
+
+        precond = lr_drv._get_precond(np.array([-0.9, -0.4, 0.2, 0.6]), 2, 4,
+                                      0.1)
+        assert precond.data.shape == (2, 2)
+        assert np.all(np.isfinite(precond.data))
--- tests/test_rpa_unrest.py
+++ tests/test_rpa_unrest.py
@@ -1,4 +1,6 @@
 import numpy as np
+import h5py
+from mpi4py import MPI
 import pytest
 
 from veloxchem.veloxchemlib import mpi_master
@@ -173,3 +175,126 @@ class TestUnrestrictedRPA:
                 assert np.max(
                     np.abs(lr_results_first[key] -
                            lr_results_second[key])) < tol
+
+    def test_restart_allows_missing_nstates(self, tmp_path):
+
+        xyz_string = """3
+        xyz
+        O   -0.1858140  -1.1749469   0.7662596
+        H   -0.1285513  -0.8984365   1.6808606
+        H   -0.0582782  -0.3702550   0.2638279
+        """
+        mol = Molecule.read_xyz_string(xyz_string)
+        mol.set_charge(1)
+        mol.set_multiplicity(2)
+        bas = MolecularBasis.read(mol, 'sto-3g', ostream=None)
+
+        scf_drv = ScfUnrestrictedDriver()
+        scf_drv.ostream.mute()
+
+        filename = str(tmp_path / 'unrestricted_missing_nstates')
+        filename = scf_drv.comm.bcast(filename, root=mpi_master())
+
+        scf_drv.filename = filename
+        scf_drv.compute(mol, bas)
+        scf_drv.restart = True
+        scf_results = scf_drv.compute(mol, bas)
+        scf_results['filename'] = filename
+
+        reference_drv = LinearResponseUnrestrictedEigenSolver()
+        reference_drv.ostream.mute()
+        reference_drv.filename = filename
+        reference_drv.conv_thresh = 1.0e-5
+        reference_drv.max_iter = 60
+        reference_drv.nstates = 2
+        reference_drv.initial_guess_multiplier = 2
+        reference_drv.max_subspace_dim = 8
+        reference_drv.ri_auxiliary_basis = 'def2-universal-jfit'
+        reference_drv.compute(mol, bas, scf_results)
+
+        if reference_drv.rank == mpi_master():
+            with h5py.File(reference_drv.checkpoint_file, 'a') as h5f:
+                del h5f['nstates']
+
+        reference_drv.comm.barrier()
+
+        restarted_drv = LinearResponseUnrestrictedEigenSolver()
+        restarted_drv.ostream.mute()
+        restarted_drv.filename = filename
+        restarted_drv.checkpoint_file = reference_drv.checkpoint_file
+        restarted_drv.conv_thresh = 1.0e-5
+        restarted_drv.max_iter = 60
+        restarted_drv.nstates = 2
+        restarted_drv.initial_guess_multiplier = 2
+        restarted_drv.max_subspace_dim = 8
+        restarted_drv.ri_auxiliary_basis = 'def2-universal-jfit'
+        restarted_drv.restart = True
+
+        restarted_results = restarted_drv.compute(mol, bas, scf_results)
+
+        assert restarted_drv.restart is True
+
+        if restarted_drv.rank == mpi_master():
+            assert restarted_results['number_of_states'] == restarted_drv.nstates
+
+    def test_guess_and_preconditioner_helpers(self):
+
+        lr_drv = LinearResponseUnrestrictedEigenSolver()
+        lr_drv.ostream.mute()
+        lr_drv.nstates = 1
+        lr_drv.initial_guess_multiplier = 2
+
+        guesses = lr_drv._initial_excitations(
+            1, (np.array([-0.8, 0.1]), np.array([-0.7, 0.2])), (1, 1), 2)
+        assert len(guesses) == 2
+        assert guesses[0].data.shape == (2, 2)
+        assert guesses[1].data.shape == (2, 2)
+
+        lr_drv.core_excitation = True
+        lr_drv.num_core_orbitals = 1
+        core_guesses = lr_drv._initial_excitations(
+            1, (np.array([-0.9, -0.4, 0.2]), np.array([-0.8, -0.3, 0.3])), (2, 2),
+            3)
+        assert len(core_guesses) == 2
+
+        precond = lr_drv._get_precond(
+            (np.array([-0.9, -0.4, 0.2]), np.array([-0.8, -0.3, 0.3])), (2, 2), 3,
+            0.1)
+        assert precond.data.shape == (2, 2)
+        assert np.all(np.isfinite(precond.data))
+
+    @pytest.mark.skipif(MPI.COMM_WORLD.Get_size() > 1,
+                        reason='skip pytest.raises for multiple MPI processes')
+    def test_rejects_unsupported_modes(self):
+
+        xyz_string = """3
+        xyz
+        O   -0.1858140  -1.1749469   0.7662596
+        H   -0.1285513  -0.8984365   1.6808606
+        H   -0.0582782  -0.3702550   0.2638279
+        """
+        mol = Molecule.read_xyz_string(xyz_string)
+        mol.set_charge(1)
+        mol.set_multiplicity(2)
+        bas = MolecularBasis.read(mol, 'sto-3g', ostream=None)
+
+        scf_drv = ScfUnrestrictedDriver()
+        scf_drv.ostream.mute()
+        scf_results = scf_drv.compute(mol, bas)
+
+        nonlinear_drv = LinearResponseUnrestrictedEigenSolver()
+        nonlinear_drv.ostream.mute()
+        nonlinear_drv.nstates = 1
+        nonlinear_drv.nonlinear = True
+
+        with pytest.raises(AssertionError, match='not implemented for nonlinear'):
+            nonlinear_drv.compute(mol, bas, scf_results)
+
+        restricted_drv = LinearResponseUnrestrictedEigenSolver()
+        restricted_drv.ostream.mute()
+        restricted_drv.nstates = 1
+        restricted_drv.restricted_subspace = True
+
+        with pytest.raises(AssertionError,
+                           match='restricted_subspace not implemented'):
+            restricted_drv.compute(mol, bas, scf_results)
