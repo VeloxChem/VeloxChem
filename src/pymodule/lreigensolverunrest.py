Fix sign of de-excitation terms in ESA transition density

src/pymodule/lreigensolverunrest.py
@@ -764,9 +764,9 @@ class LinearResponseUnrestrictedEigenSolver(LinearResponseEigenSolverBase):
 
                             esa_trans_dens += (
                                 np.linalg.multi_dot(
-                                    [mo_occ, y_mat_1, y_mat_2.T, mo_occ.T]) -
+                                    [mo_vir, y_mat_1.T, y_mat_2, mo_vir.T]) -
                                 np.linalg.multi_dot(
-                                    [mo_vir, y_mat_1.T, y_mat_2, mo_vir.T]))
+                                    [mo_occ, y_mat_1, y_mat_2.T, mo_occ.T]))
 
                             esa_trans_dipole = np.array([
                                 np.sum(esa_trans_dens * dipole_integrals[i])

tests/test_rpa.py
@@ -222,10 +222,10 @@ class TestRPA:
             esa_results = lr_results['esa_results']
 
             ref_excitation_energies = np.array([0.0706309931, 0.0887848965])
-            ref_oscillator_strengths = np.array([0.1379665336, 0.0013975526])
+            ref_oscillator_strengths = np.array([0.1379630687, 0.0013910509])
             ref_transition_dipoles = np.array([
-                [-0.0794559666, -0.5972010928, 1.6022021187],
-                [0.1515690613, -0.0251919303, -0.0018734103],
+                [-0.0794549688, -0.5971935937, 1.6021820000],
+                [0.1512160878, -0.0251332634, -0.0018690476],
             ])
 
             src_states = [item['from_state'] for item in esa_results]