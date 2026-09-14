Fix sign of de-excitation terms in RIXS transition density

src/pymodule/rixsdriver.py
@@ -850,8 +850,7 @@ class RixsDriver(LinearSolver):
 
         return pp_core_eigvecs
 
-    @staticmethod
-    def get_tdms(mo_occ, mo_vir, z_val, y_val, z_core, y_core):
+    def get_tdms(self, mo_occ, mo_vir, z_val, y_val, z_core, y_core):
         """
         Get the transition density matrices, both from ground-state
         to excited state, and between two excited states (here between)
@@ -861,25 +860,25 @@ class RixsDriver(LinearSolver):
             The occupied molecular orbitals.
         :param mo_vir:
             The unoccupied/virtual molecular orbitals.
-        :z_val:
+        :param z_val:
             The excitation matrix (valence-excited state).
-        :y_val:
+        :param y_val:
             The dexcitation matrix (valence-excited state).
-        :z_core:
+        :param z_core:
             The excitation matrix (core-excited state).
-        :y_core:
+        :param y_core:
             The dexcitation matrix (core-excited state).
+
+        :return:
+            The ground-to-core and core-to-valence transition densities.
         """
 
         gs_to_core = np.linalg.multi_dot([mo_occ, z_core - y_core, mo_vir.T])
         gs_to_core *= np.sqrt(2.0)
 
-        core_to_val = (
-            np.linalg.multi_dot([mo_vir, z_val.T, z_core, mo_vir.T]) -
-            np.linalg.multi_dot([mo_occ, z_val, z_core.T, mo_occ.T]) +
-            np.linalg.multi_dot([mo_occ, y_val, y_core.T, mo_occ.T]) -
-            np.linalg.multi_dot([mo_vir, y_val.T, y_core, mo_vir.T])
-        )
+        core_to_val = self._get_esa_transition_density(
+            z_core, y_core, z_val, y_val, mo_occ, mo_vir)
+
         return gs_to_core, core_to_val
 
     def _print_header(self, molecule, basis):